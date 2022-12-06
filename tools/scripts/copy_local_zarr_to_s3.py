ae7f481c2066806cb4916785026f80af2682287c
Matthew Hartley <matthewh@ebi.ac.uk>
2022-12-06 19:57:46 +0000

    Add script for copying local Zarr files to S3

---
+++ tools/scripts/copy_local_zarr_to_s3.py
@@ -0,0 +1,24 @@
+import logging
+from pathlib import Path
+
+import click
+
+from bia_integrator_tools.io import copy_local_zarr_to_s3
+
+
+logging.getLogger(__file__)
+
+
+@click.command()
+@click.argument("zarr_fpath")
+@click.argument("accession_id")
+@click.argument("image_id")
+def main(zarr_fpath, accession_id, image_id):
+
+    logging.basicConfig(level=logging.INFO)
+
+    copy_local_zarr_to_s3(Path(zarr_fpath), accession_id, image_id)
+
+
+if __name__ == "__main__":
+    main()
