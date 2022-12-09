8712c0aacab52c4263925b0df135f56f48950029
Matthew Hartley <matthewh@ebi.ac.uk>
2022-12-09 13:35:46 +0000

    Add zip handling scripts

--- tools/scripts/bst_pulldown.py
+++ tools/scripts/bst_pulldown.py
@@ -2,11 +2,13 @@ import logging
 from typing import List, Optional
 
 import click
-from bia_integrator_core.models import BIAStudy, BIAImage, BIAImageRepresentation
+from bia_integrator_core.models import BIAFile, BIAStudy, BIAImage, BIAImageRepresentation, Author, BIAFileRepresentation
 from bia_integrator_core.study import persist_study
 
 from bia_integrator_tools.biostudies import (
+    File,
     Submission,
+    Section,
     attributes_to_dict,
     load_submission,
     file_uri,
@@ -17,6 +19,27 @@ logger = logging.getLogger(__file__)
 
 
 IMAGE_EXTS = ['.png', '.czi', '.tif', '.lsm', '.ims', '.lif', '.tiff', '.nd2', '.ome.tiff', '.dv', '.svs']
+ARCHIVE_EXTS = ['.zip']
+
+
+def is_image(file: File) -> bool:
+    return file.path.suffix in IMAGE_EXTS
+
+
+def author_section_to_author(author_section: Section) -> Author:
+    attributes_dict = attributes_to_dict(author_section.attributes)
+    
+    return Author(name=attributes_dict['Name'])
+
+
+def find_authors_in_submission(submission: Submission):
+    
+    authors = []
+    for section in submission.section.subsections:
+        if section.type == 'author':
+            authors.append(author_section_to_author(section))
+            
+    return authors
 
 
 def bst_submission_to_bia_study(submission: Submission) -> BIAStudy:
@@ -39,13 +62,19 @@ def bst_submission_to_bia_study(submission: Submission) -> BIAStudy:
     accession_id = submission.accno
     
     all_files = find_files_in_submission(submission)
-    image_files = [
-        file
-        for file in all_files
-        if is_image(file)
-    ]
+    image_files = []
+    other_files = []
+    archive_files = []
 
-    logger.info(f"Submission has {len(all_files)} files, of which {len(image_files)} are images.")
+    for file in all_files:
+        if file.path.suffix in IMAGE_EXTS:
+            image_files.append(file)
+        elif file.path.suffix in ARCHIVE_EXTS:
+            archive_files.append(file)
+        else:
+            other_files.append(file)
+
+    logger.info(f"Submission has {len(all_files)} files, of which {len(image_files)} are images and {len(archive_files)} are archive files.")
 
     images = {}
     for n, imfile in enumerate(image_files, start=1):
@@ -56,7 +85,8 @@ def bst_submission_to_bia_study(submission: Submission) -> BIAStudy:
             uri=file_uri(accession_id, imfile),
             size=imfile.size,
             type="fire_object",
-            dimensions=None
+            dimensions=None,
+            attributes = {}
         )
         images[image_id] = BIAImage(
             dimensions=None,
@@ -73,6 +103,7 @@ def bst_submission_to_bia_study(submission: Submission) -> BIAStudy:
     bia_study = BIAStudy(
         accession_id=accession_id,
         title=study_title,
+        authors=find_authors_in_submission(submission),
         release_date=submission_attr_dict['ReleaseDate'],
         description=study_section_attr_dict['Description'],
         organism=study_section_attr_dict.get('Organism', "Unknown"),
@@ -81,11 +112,48 @@ def bst_submission_to_bia_study(submission: Submission) -> BIAStudy:
         images=images
     )
 
+    archivefiles = {}
+    for n, file in enumerate(archive_files, start=1):
+        archive_id = f"Z{n}"
+        archivefiles[archive_id] = BIAFile(
+            id=archive_id,
+            original_relpath=file.path,
+            original_size=file.size,
+            representations=[
+                BIAFileRepresentation(
+                    accession_id=accession_id,
+                    file_id=archive_id,
+                    uri=file_uri(accession_id, file),
+                    size=file.size
+                )
+            ],
+            attributes=attributes_to_dict(file.attributes)
+        )
+    bia_study.archive_files = archivefiles
+
+    # FIXME - horrible variable naming, but it matches the images case above
+    otherfiles = {}
+    for n, otherfile in enumerate(other_files, start=1):
+        other_id = f"O{n}"
+        otherfiles[other_id] = BIAFile(
+            id=other_id,
+            original_relpath=otherfile.path,
+            original_size=otherfile.size,
+            representations=[
+                BIAFileRepresentation(
+                    accession_id=accession_id,
+                    file_id=other_id,
+                    uri=file_uri(accession_id, otherfile),
+                    size=otherfile.size
+                )
+            ],
+            attributes=attributes_to_dict(otherfile.attributes)
+        )
+    bia_study.other_files = otherfiles
+
+
+
     return bia_study
-
-
-def is_image(file):
-    return file.path.suffix in IMAGE_EXTS
 
 
 @click.command()
