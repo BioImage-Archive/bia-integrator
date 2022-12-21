18644bce1c05b9ec52bfc7ad8ea16a9d5187d540
Matthew Hartley <matthewh@ebi.ac.uk>
2022-12-21 18:19:36 +0000

    Add conversion settings

--- tools/bia_integrator_tools/conversion.py
+++ tools/bia_integrator_tools/conversion.py
@@ -1,20 +1,27 @@
-import os
 import logging
 import subprocess
 
+from pydantic import BaseSettings
 
-BIOFORMATS2RAW_JAVA_HOME = os.environ["BIOFORMATS2RAW_JAVA_HOME"]
-BIOFORMATS2RAW_BIN = os.environ["BIOFORMATS2RAW_BIN"]
+class ConversionSettings(BaseSettings):
+    bioformats2raw_java_home: str
+    bioformats2raw_bin: str
+
+    class Config:
+        env_file = '.env'
 
 
 logger = logging.getLogger(__name__)
+
+
+settings = ConversionSettings()
 
 
 def run_zarr_conversion(input_fpath, output_dirpath):
     """Convert the local file at input_fpath to Zarr format, in a directory specified by
     output_dirpath"""
 
-    zarr_cmd = f'export JAVA_HOME={BIOFORMATS2RAW_JAVA_HOME} && {BIOFORMATS2RAW_BIN} "{input_fpath}" "{output_dirpath}"'
+    zarr_cmd = f'export JAVA_HOME={settings.bioformats2raw_java_home} && {settings.bioformats2raw_bin} "{input_fpath}" "{output_dirpath}"'
 
     logger.info(f"Converting with {zarr_cmd}")
     retval = subprocess.run(zarr_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
