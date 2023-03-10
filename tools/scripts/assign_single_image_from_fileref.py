12bcdf750a9d39379cd2c114b9bc11eb7be135fb
Matthew Hartley <matthewh@ebi.ac.uk>
2023-03-10 10:46:39 +0000

    Add assignment/conversion helper scripts

--- tools/scripts/assign_single_image_from_fileref.py
+++ tools/scripts/assign_single_image_from_fileref.py
@@ -12,15 +12,7 @@ from bia_integrator_core.image import persist_image
 logger = logging.getLogger(__file__)
 
 
-@click.command()
-@click.argument("accession_id")
-@click.argument("fileref_id")
-def main(accession_id, fileref_id):
-    logging.basicConfig(level=logging.INFO)
-
-    bia_study = load_and_annotate_study(accession_id)
-
-    fileref = bia_study.file_references[fileref_id]
+def create_and_persist_image_from_fileref(accession_id, fileref):
     name = fileref.name
     logger.info(f"Assigned name {name}")
 
@@ -34,6 +26,7 @@ def main(accession_id, fileref_id):
         image_id=image_id,
         size=fileref.size_in_bytes,
         uri=fileref.uri,
+        attributes={"fileref_ids": [fileref.id]},
         type="fire_object"
     )
 
@@ -46,6 +39,23 @@ def main(accession_id, fileref_id):
     )
 
     persist_image(image)
+
+    return image_id
+
+
+@click.command()
+@click.argument("accession_id")
+@click.argument("fileref_id")
+def main(accession_id, fileref_id):
+    logging.basicConfig(level=logging.INFO)
+
+    bia_study = load_and_annotate_study(accession_id)
+
+    fileref = bia_study.file_references[fileref_id]
+
+
+    create_and_persist_image_from_fileref(accession_id, fileref)
+
 
         
 
--- tools/scripts/convert_local.py
+++ tools/scripts/convert_local.py
@@ -14,71 +14,32 @@ from bia_integrator_tools.conversion import run_zarr_conversion
 from bia_integrator_core.integrator import load_and_annotate_study
 from bia_integrator_core.models import BIAImageRepresentation
 from bia_integrator_core.interface import persist_image_representation
+from bia_integrator_tools.utils import get_image_rep_by_type
+from bia_integrator_tools.io import stage_fileref_and_get_fpath, c2zsettings
 
 
 logger = logging.getLogger(__file__)
 
 
-# COMPANION_TEMPLATE = """<?xml version='1.0' encoding='utf-8'?>
-# <OME xmlns="http://www.openmicroscopy.org/Schemas/OME/2016-06" xmlns:OME="http://www.openmicroscopy.org/Schemas/OME/2016-06" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.openmicroscopy.org/Schemas/OME/2016-06 http://www.openmicroscopy.org/Schemas/OME/2016-06/ome.xsd">
-#   <Image ID="Image:0" Name="{image_name}">
-#     <Pixels DimensionOrder="XYZCT" ID="Pixels:0:0" SizeC="1" SizeT="1" SizeX="{size_x}" SizeY="{size_y}" SizeZ="{size_z}" Type="{dtype}">
-#       <TiffData FirstC="0" FirstT="0" FirstZ="0" PlaneCount="{n_planes}">
-#         <UUID FileName="{filename}">urn:uuid:{uuid}</UUID>
-#       </TiffData>
-#     </Pixels>
-#   </Image>
-# </OME>
-# """
-
-COMPANION_TEMPLATE = """<?xml version='1.0' encoding='utf-8'?>
-<OME xmlns="http://www.openmicroscopy.org/Schemas/OME/2016-06" xmlns:OME="http://www.openmicroscopy.org/Schemas/OME/2016-06" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.openmicroscopy.org/Schemas/OME/2016-06 http://www.openmicroscopy.org/Schemas/OME/2016-06/ome.xsd">
-  <Image ID="Image:0" Name="{image_name}">
-    <Pixels DimensionOrder="XYZCT" ID="Pixels:0:0" SizeC="1" SizeT="1" SizeX="{size_x}" SizeY="{size_y}" SizeZ="{size_z}" Type="uint16">
-      <TiffData FirstC="0" FirstT="0" FirstZ="0" PlaneCount="{n_planes}">
-        <UUID FileName="{filename}">urn:uuid:{uuid}</UUID>
-      </TiffData>
-    </Pixels>
-  </Image>
-</OME>
-"""
-
 @click.command()
-@click.argument("input_fpath")
-def main(input_fpath):
-
-    import sys; sys.exit(0)
+@click.argument("accession_id")
+@click.argument("image_id")
+@click.argument("output_fpath")
+def main(accession_id, image_id, output_fpath):
 
     logging.basicConfig(level=logging.INFO)
 
-    from imageio import volread
+    bia_study = load_and_annotate_study(accession_id)
 
-    vol = volread(input_fpath)
+    rep = get_image_rep_by_type(accession_id, image_id, "fire_object")
+    fileref_id = rep.attributes["fileref_ids"][0]
+    fileref = bia_study.file_references[fileref_id]
+    input_fpath = stage_fileref_and_get_fpath(accession_id, fileref)
 
-    size_x, size_y, size_z = vol.shape
-    n_planes=size_z
-
-    fname = Path(input_fpath).name
-
-    companion_str = COMPANION_TEMPLATE.format(
-        image_name=fname,
-        size_x=size_x,
-        size_y=size_y,
-        size_z=size_z,
-        n_planes=n_planes,
-        dtype=vol.dtype,
-        filename=fname,
-        uuid=uuid.uuid1()
-    )
+    run_zarr_conversion(input_fpath, output_fpath)
 
 
-    with tempfile.TemporaryDirectory() as tmpdir:
-        tmpdir_path = Path(tmpdir)
-        tmpfpath = tmpdir_path/"tmp.companion.ome"
-        os.symlink(input_fpath, tmpdir_path/fname)
-        with open(tmpfpath, "w") as fh:
-            fh.write(companion_str)
-        run_zarr_conversion(tmpfpath, "tmp/IM2.zarr")
+
 
 
 if __name__ == "__main__":
