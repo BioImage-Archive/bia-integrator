dcd6ec35941d201677172bbda92ed9d184ab584e
Aybuke <aybuke@ebi.ac.uk>
2023-06-15 14:36:06 +0100

    updated selecting script

--- tools/scripts/select_n_images_to_convert.py
+++ tools/scripts/select_n_images_to_convert.py
@@ -17,7 +17,7 @@ def main(accession_id,number_of_images):
     logging.basicConfig(level=logging.INFO)
 
     bia_study = load_and_annotate_study(accession_id)
-    image_ids = bia_study.images.keys()
+    image_ids = list(bia_study.images.keys())
     n = int(number_of_images)
     print(','.join(image_ids[:n]))
 
