4a8ef380568fe2bde59b0dd2b24101a270f84f7a
Matthew Hartley <matthewh@ebi.ac.uk>
2023-06-26 11:55:56 +0100

    Fix scripts that use rendering code

--- tools/scripts/generate_thumbnail.py
+++ tools/scripts/generate_thumbnail.py
@@ -7,42 +7,18 @@ from bia_integrator_core.interface import persist_image_representation
 from bia_integrator_tools.utils import get_ome_ngff_rep_by_accession_and_image
 from bia_integrator_tools.io import copy_local_to_s3
 
-from preview import (
-    get_zarr_uri, min_dim_array_from_zarr_uri, get_single_channel_image_arrays,
-    channel_render_to_cmap,
-    pad_to_target_dims,
-    channel_arrays_and_chrenders_to_thumbnail,
-    scale_channel_arrays
-)
+from bia_integrator_tools.rendering import generate_padded_thumbnail_from_ngff_uri
+
 
 logger = logging.getLogger(__file__)
 
 
 def generate_and_persist_thumbnail_from_ngff_rep(ome_ngff_rep, dimensions):
+
     accession_id = ome_ngff_rep.accession_id
     image_id = ome_ngff_rep.image_id
 
-    rendering = ome_ngff_rep.rendering
-
-    if not rendering:
-        render = ChannelRendering(
-            colormap_start=[0., 0., 0.],
-            colormap_end=[1., 1., 1.],
-            scale_factor=1
-        )
-        rendering=RenderingInfo(
-            channel_renders=[render],
-            default_t=None,
-            default_z=None
-        )
-
-    channel_renders = rendering.channel_renders
-    imarray = min_dim_array_from_zarr_uri(ome_ngff_rep.uri, dimensions)
-    channel_arrays = get_single_channel_image_arrays(imarray, rendering.default_z, rendering.default_t)
-    scaled_arrays = scale_channel_arrays(channel_arrays, channel_renders)
-
-    im = channel_arrays_and_chrenders_to_thumbnail(scaled_arrays, channel_renders, dimensions)
-
+    im = generate_padded_thumbnail_from_ngff_uri(ome_ngff_rep.uri)
     w, h = dimensions
 
     dst_key = f"{accession_id}/{image_id}/{image_id}-thumbnail-{w}-{h}.png"
@@ -77,7 +53,7 @@ def main(accession_id, image_id):
 
     ome_ngff_rep = get_ome_ngff_rep_by_accession_and_image(accession_id, image_id)
 
-    generate_and_persist_thumbnail_from_ngff_rep(ome_ngff_rep, dimensions   )
+    generate_and_persist_thumbnail_from_ngff_rep(ome_ngff_rep, dimensions)
 
 
 if __name__ == "__main__":
---
+++ tools/scripts/render_image_to_local_file.py
@@ -0,0 +1,23 @@
+import typer
+
+from bia_integrator_tools.utils import get_ome_ngff_rep_by_accession_and_image
+from bia_integrator_tools.rendering import generate_padded_thumbnail_from_ngff_uri
+
+
+app = typer.Typer()
+
+
+@app.command()
+def main(accession_id: str, image_id: str, output_fname: str):
+    ome_ngff_rep = get_ome_ngff_rep_by_accession_and_image(accession_id, image_id)
+
+    im = generate_padded_thumbnail_from_ngff_uri(ome_ngff_rep.uri)
+
+    im.save(output_fname)
+
+
+
+
+
+if __name__ == "__main__":
+    app()
