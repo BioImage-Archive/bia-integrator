1f6239a6cb88ffd41f8a382e30445194d63ce373
Liviu Anita <liviu@ebi.ac.uk>
2023-10-02 17:46:12 +0100

    Fix config

--- core/bia_integrator_core/config.py
+++ core/bia_integrator_core/config.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 from pydantic import BaseSettings
 from typing import Optional
-from openapi_client.util import simple_client
-from openapi_client.api import DefaultApi
+from bia_integrator_api.util import simple_client
+from bia_integrator_api.api import PrivateApi
 
 class Settings(BaseSettings):
     bia_api_basepath: str = "http://localhost:8080"
@@ -11,10 +11,10 @@ class Settings(BaseSettings):
     bia_disable_ssl_host_check: bool = False
     cache_root_dirpath: Path = Path.home()/".cache"/"bia-converter"
 
-    bia_api_client: Optional[DefaultApi] = None
+    bia_api_client: Optional[PrivateApi] = None
 
     @property
-    def api_client(self) -> DefaultApi:
+    def api_client(self) -> PrivateApi:
         if not self.bia_api_client:
             self.bia_api_client = simple_client(
                 self.bia_api_basepath,
