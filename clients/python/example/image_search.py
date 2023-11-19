436cf7e4eda1ecf8928850d0245b7e0aab5aad6d
Liviu Anita <liviu@ebi.ac.uk>
2023-11-19 15:19:28 +0000

    Update image search example

--- clients/python/example/image_search.py
+++ clients/python/example/image_search.py
@@ -3,18 +3,24 @@ from bia_integrator_api import models as api_models
 import os
 
 rw_client = simple_client(
-    api_base_url = "https://bia-cron-1.ebi.ac.uk:8080/",
+    api_base_url = "https://bia-cron-1.ebi.ac.uk:8080",
     # FIXME: Re-enable and remove this flag once api is on publicly accessible infra
     disable_ssl_host_check = True
 )
 
-rw_client.search_images_exact_match(
-    
+search_result = rw_client.search_images_exact_match(
+    api_models.SearchImageFilter(
+        annotations_any=[api_models.SearchAnnotation(
+            key="dimension_order",
+            value="XYZCT"
+        )],
+        image_representations_any=[api_models.SearchFileRepresentation(
+            type="thumbnail",
+            size_bounds_lte=1000000000
+        )],
+        study_uuid="00000000-0000-0000-0006-09b5dbf57bdf",
+        limit=10
+    )
 )
 
-{
-    "annotations_any": [{"dimension_order": "XYZCT"} ],
-    "image_representations_any": [{"type": "thumbnail", "size_lte": 1000000000} ],
-    "study_uuid": "00000000-0000-0000-0006-09b5dbf57bdf",
-    "limit": 10
-}
+print(search_result)
