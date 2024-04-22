70f49114536de594543d6ef898775a80a578e939
Liviu Anita <liviu@ebi.ac.uk>
2024-04-22 15:50:52 +0100

    Fix race condition when bd gets reused

--- api/src/api/private.py
+++ api/src/api/private.py
@@ -22,7 +22,7 @@ router = APIRouter(
 @router.post("/studies", status_code=status.HTTP_201_CREATED)
 async def create_study(
     study: db_models.BIAStudy,
-    db: Repository = Depends(),
+    db: Repository = Depends(use_cache=False),
     overwrite_mode: OverwriteMode = OverwriteMode.FAIL,
 ) -> None:
     logging.info(f"Creating study {study.accession_id}")
@@ -37,7 +37,9 @@ async def create_study(
 
 
 @router.patch("/studies", status_code=status.HTTP_200_OK)
-async def update_study(study: db_models.BIAStudy, db: Repository = Depends()) -> None:
+async def update_study(
+    study: db_models.BIAStudy, db: Repository = Depends(use_cache=False)
+) -> None:
     logging.info(f"Updating study {study.accession_id}. New version: {study.version}")
     await db.update_doc(study)
 
@@ -47,7 +49,9 @@ async def update_study(study: db_models.BIAStudy, db: Repository = Depends()) ->
 @router.post(
     "/studies/{study_uuid}/refresh_counts", status_code=status.HTTP_201_CREATED
 )
-async def study_refresh_counts(study_uuid: str, db: Repository = Depends()) -> None:
+async def study_refresh_counts(
+    study_uuid: str, db: Repository = Depends(use_cache=False)
+) -> None:
     """Recalculate and persist counts for other objects pointing to this study."""
 
     logging.info(f"Recalculating reference counts for study {study_uuid}")
@@ -60,7 +64,7 @@ async def study_refresh_counts(study_uuid: str, db: Repository = Depends()) -> N
 async def create_images(
     study_images: List[db_models.BIAImage],
     response: Response,
-    db: Repository = Depends(),
+    db: Repository = Depends(use_cache=False),
     overwrite_mode: OverwriteMode = OverwriteMode.FAIL,
 ) -> api_models.BulkOperationResponse:
     logging.info(
@@ -97,7 +101,7 @@ async def create_images(
 
 @router.patch("/images/single", status_code=status.HTTP_200_OK)
 async def update_image(
-    study_image: db_models.BIAImage, db: Repository = Depends()
+    study_image: db_models.BIAImage, db: Repository = Depends(use_cache=False)
 ) -> None:
     """Bulk update not available - update_many only has one filter for the entire update
     @TODO: Find common bulk update usecases and map them to mongo operations"""
@@ -130,7 +134,7 @@ async def create_images_bulk() -> None:
 async def create_image_representation(
     image_uuid: str,
     representation: db_models.BIAImageRepresentation,
-    db: Repository = Depends(),
+    db: Repository = Depends(use_cache=False),
 ) -> None:
     logging.info(f"Adding an image representation to image {image_uuid}")
     await db.list_item_push(image_uuid, "representations", representation)
@@ -142,7 +146,7 @@ async def create_image_representation(
 async def create_file_references(
     file_references: List[db_models.FileReference],
     response: Response,
-    db: Repository = Depends(),
+    db: Repository = Depends(use_cache=False),
     overwrite_mode: OverwriteMode = OverwriteMode.FAIL,
 ) -> api_models.BulkOperationResponse:
     logging.info(
@@ -178,7 +182,7 @@ async def create_file_references(
 
 @router.patch("/file_references/single", status_code=status.HTTP_200_OK)
 async def update_file_reference(
-    file_reference: db_models.FileReference, db: Repository = Depends()
+    file_reference: db_models.FileReference, db: Repository = Depends(use_cache=False)
 ) -> None:
     logging.info(
         f"Updating file reference {file_reference.uuid}. New version: {file_reference.version}"
@@ -198,7 +202,7 @@ async def update_file_reference(
 @router.post("/collections", status_code=status.HTTP_201_CREATED)
 async def create_collection(
     collection: db_models.BIACollection,
-    db: Repository = Depends(),
+    db: Repository = Depends(use_cache=False),
     overwrite_mode: OverwriteMode = OverwriteMode.FAIL,
 ) -> None:
     logging.info(f"Creating collection {collection.uuid}")
@@ -217,7 +221,7 @@ async def create_collection(
 @router.post("/image_acquisitions", status_code=status.HTTP_201_CREATED)
 async def create_image_acquisition(
     image_acquisition: db_models.ImageAcquisition,
-    db: Repository = Depends(),
+    db: Repository = Depends(use_cache=False),
     overwrite_mode: OverwriteMode = OverwriteMode.FAIL,
 ) -> None:
     logging.info(f"Creating Image Acquisition {image_acquisition.uuid}")
@@ -236,7 +240,8 @@ async def create_image_acquisition(
 
 @router.patch("/image_acquisitions", status_code=status.HTTP_200_OK)
 async def update_image_acquisition(
-    image_acquisition: db_models.ImageAcquisition, db: Repository = Depends()
+    image_acquisition: db_models.ImageAcquisition,
+    db: Repository = Depends(use_cache=False),
 ) -> None:
     logging.info(
         f"Updating Image acquisition {image_acquisition.uuid}. New version: {image_acquisition.version}"
@@ -275,7 +280,7 @@ async def create_specimen(
 
 @router.patch("/specimens", status_code=status.HTTP_200_OK)
 async def update_specimen(
-    specimen: db_models.Specimen, db: Repository = Depends()
+    specimen: db_models.Specimen, db: Repository = Depends(use_cache=False)
 ) -> None:
     logging.info(f"Updating Specimen {specimen.uuid}. New version: {specimen.version}")
     # await db.validate_uuid_type(specimen.biosample_uuid, db_models.Biosample)
@@ -293,7 +298,7 @@ async def update_specimen(
 @router.post("/biosamples", status_code=status.HTTP_201_CREATED)
 async def create_biosample(
     biosample: db_models.Biosample,
-    db: Repository = Depends(),
+    db: Repository = Depends(use_cache=False),
     overwrite_mode: OverwriteMode = OverwriteMode.FAIL,
 ) -> None:
     logging.info(f"Creating Biosample {biosample.uuid}")
@@ -305,7 +310,7 @@ async def create_biosample(
 
 @router.patch("/biosamples", status_code=status.HTTP_200_OK)
 async def update_biosample(
-    biosample: db_models.Biosample, db: Repository = Depends()
+    biosample: db_models.Biosample, db: Repository = Depends(use_cache=False)
 ) -> None:
     logging.info(
         f"Updating Biosample {biosample.uuid}. New version: {biosample.version}"
@@ -317,7 +322,9 @@ async def update_biosample(
 
 @router.post("/images/{image_uuid}/ome_metadata", status_code=status.HTTP_201_CREATED)
 async def set_image_ome_metadata(
-    image_uuid: UUID, ome_metadata_file: UploadFile, db: Repository = Depends()
+    image_uuid: UUID,
+    ome_metadata_file: UploadFile,
+    db: Repository = Depends(use_cache=False),
 ) -> db_models.BIAImageOmeMetadata:
     if not ome_metadata_file.size:
         raise exceptions.InvalidRequestException("File has size 0")
