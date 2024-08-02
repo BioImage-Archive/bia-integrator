778cf9c907511c7afc823447613df1a1837712d2
sherwoodf <161822064+sherwoodf@users.noreply.github.com>
2024-08-02 14:32:38 +0100

    added export of associated objects from dataset (#137)
    * added export of associated objects from dataset
    
    * actuall commit changes to the test result
    
    * fix spelling of rea*d*_all_json function
    
    * fix specimen_growth_protocol class

--- bia-export/bia_export/website_conversion.py
+++ bia-export/bia_export/website_conversion.py
@@ -7,44 +7,101 @@ from .website_models import (
     ExperimentalImagingDataset
 )
 from glob import glob
-from typing import List
+from typing import List, Type
+from bia_shared_datamodels import bia_data_model
+from pydantic import BaseModel
 
 logger = logging.getLogger(__name__)
+
+
+def read_api_json_file(file_path: Path, object_type: Type[BaseModel]) -> BaseModel:
+    """
+    Returns model of object from file to be equivalent to using BIA API Client
+    """
+    with open(file_path, "r") as object_file:
+        object_dict = json.load(object_file)
+    
+    return object_type(**object_dict)
+
+
+def read_all_json(directory_path: Path, object_type: Type[BaseModel]) -> List[BaseModel]:
+    object_list = []
+    file_paths = glob(str(directory_path))
+    for file_path in file_paths:
+        object_list.append(read_api_json_file(file_path, object_type))
+    return object_list
+
+
+def find_associated_objects(typed_associations: set, directory_path: Path, object_type: Type[BaseModel]) -> List[bia_data_model.UserIdentifiedObject]:
+    linked_object = []
+
+    if len(typed_associations) == 0:
+        return linked_object
+    
+    typed_object_in_study: List[bia_data_model.UserIdentifiedObject] = read_all_json(directory_path, object_type)
+    for object in typed_object_in_study:
+        if object.title_id in typed_associations:
+            linked_object.append(object)
+    return linked_object
+
 
 def create_study(
         accession_id: str,
         root_directory: Path
 ) -> Study:
+    
     if root_directory:
         study_path = root_directory.joinpath(f'studies/{accession_id}.json')
 
         logger.info(f'Loading study from {study_path}')
         
-        with open(study_path, "r") as study_file:
-            study_dict = json.load(study_file)
-        
-        study_dict["experimental_imaging_component"] = convert_experimental_imaging_datasets(accession_id, root_directory)
+        api_study = read_api_json_file(study_path, bia_data_model.Study)
+    else:
+        #TODO: use client
+        raise NotImplementedError
+    study_dict = api_study.model_dump()
+    study_dict["experimental_imaging_component"] = create_experimental_imaging_datasets(accession_id, root_directory)
+    study = Study(**study_dict)
 
-        study = Study(**study_dict)
-
-        return study
+    return study
 
 
-def convert_experimental_imaging_datasets(accession_id: str, root_directory: Path = None) -> List[ExperimentalImagingDataset]:
-    datasets = []
+def create_experimental_imaging_datasets(accession_id: str, root_directory: Path = None) -> List[ExperimentalImagingDataset]:
+    eid_list = []
     if root_directory:
-
-        eid_directory = root_directory.joinpath(f'experimental_imaging_datasets/{accession_id}/*.json')
-        eid_paths = glob(str(eid_directory))
-
-        for eid_path in eid_paths:
-
-            logger.info(f'Loading study from {eid_path}')
-
-            with open(eid_path, "r") as eid_file:
-                eid_dict = json.load(eid_file)
-            eid = ExperimentalImagingDataset(**eid_dict)
-            datasets.append(eid)
     
-    return datasets
+        eid_directory = root_directory.joinpath(f'experimental_imaging_datasets/{accession_id}/*.json')
+
+        api_eids: List[bia_data_model.ExperimentalImagingDataset] = read_all_json(eid_directory, bia_data_model.ExperimentalImagingDataset)
+        
+        for eid in api_eids:
+            eid_dict = eid.model_dump()
+
+            associations = eid.attribute["associations"]
+
+            association_by_type = {"biosample": set(), "image_acquisition": set(), "specimen": set()}
+            for association in associations:
+                for key in association_by_type.keys():
+                    association_by_type[key].add(association[key])
+
+
+            biosample_directory = root_directory.joinpath(f'biosamples/{accession_id}/*.json')
+            sipp_directory = root_directory.joinpath(f'specimen_imaging_preparation_protocols/{accession_id}/*.json')
+            sgp_directory = root_directory.joinpath(f'specimen_growth_protocols/{accession_id}/*.json')
+            ia_directory = root_directory.joinpath(f'image_acquisitions/{accession_id}/*.json')
+
+            biosample_linked_to_dataset = find_associated_objects(association_by_type["biosample"], biosample_directory, bia_data_model.BioSample)
+            sipp_linked_to_dataset = find_associated_objects(association_by_type["specimen"], sipp_directory, bia_data_model.SpecimenImagingPrepartionProtocol)
+            sgps_linked_to_dataset  = find_associated_objects(association_by_type["specimen"], sgp_directory, bia_data_model.SpecimenGrowthProtocol)
+            ia_linked_to_dataset  = find_associated_objects(association_by_type["image_acquisition"], ia_directory, bia_data_model.ImageAcquisition)
+
+            eid_dict["biological_entity"] = biosample_linked_to_dataset
+            eid_dict["specimen_imaging_preparation_protocol"] = sipp_linked_to_dataset
+            eid_dict["specimen_growth_protocol"] = sgps_linked_to_dataset
+            eid_dict["acquisition_process"] = ia_linked_to_dataset
+
+            eid = ExperimentalImagingDataset(**eid_dict)
+            eid_list.append(eid)
+        
+    return eid_list
             
--- bia-export/bia_export/website_models.py
+++ bia-export/bia_export/website_models.py
@@ -8,4 +8,16 @@ class Study(bia_data_model.Study):
     experimental_imaging_component: Optional[List[ExperimentalImagingDataset]] = Field(default_factory=list, description="""A dataset of that is associated with the study.""")
 
 class ExperimentalImagingDataset(bia_data_model.ExperimentalImagingDataset):
-    pass
+    acquisition_process: list[bia_data_model.ImageAcquisition] = Field(
+        description="""Processes involved in the creation of the images and files in this dataset."""
+    )
+    specimen_imaging_preparation_protocol: list[bia_data_model.SpecimenImagingPrepartionProtocol] = Field(
+        description="""Processes involved in the preprapartion of the samples for imaged."""
+    )
+    biological_entity: list[bia_data_model.BioSample] = Field(
+        description="""The biological entity or entities that were imaged."""
+    )
+    specimen_growth_protocol: Optional[list[bia_data_model.SpecimenGrowthProtocol]] = Field(
+        description="""Processes involved in the growth of the samples that were then imaged."""
+    )
+
