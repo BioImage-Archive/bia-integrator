5391daf4efc44dc6d745bcdbe5b7169bca1f3e28
Kola <kola@ebi.ac.uk>
2024-08-09 12:03:21 +0100

    Try fixing CI/CD errors on windows by manually joining parts of Path

--- bia-ingest-shared-models/bia_ingest_sm/biostudies.py
+++ bia-ingest-shared-models/bia_ingest_sm/biostudies.py
@@ -247,7 +247,16 @@ def file_uri(
     """For a given accession and file object, return the HTTP URI where we can expect
     to be able to access that file."""
 
-    return file_uri_template.format(accession_id=accession_id, relpath=file.path.as_posix())
+    # KB 09/08/2024 we are having an issue with url containing '\' when
+    # testing on windows during CI/CD.
+    # Use of PosixPath does not work -> not implemented error. Use of
+    # Path.as_posix() as commented out below does not seem to work either!
+    #return file_uri_template.format(accession_id=accession_id, relpath=file.path.as_posix())
+
+    # Hence split and manually rejoin
+    relpath = "/".join(file.path.parts)
+    return file_uri_template.format(accession_id=accession_id, relpath=relpath)
+
 
 
 def get_file_uri_template_for_accession(accession_id: str) -> str:
