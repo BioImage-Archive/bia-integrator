57c93de706348f6dea841c9b96bbaefa33356fc7
sherwoodf <161822064+sherwoodf@users.noreply.github.com>
2024-08-12 14:01:41 +0100

    added default_open field to details section of datasets (#140)
    * added default_open field to details section of datasets
    
    * forgot to add additional experimental imaging dataset file
    
    * test pytest testing. Revert me
    
    * Revert "test pytest testing. Revert me"
    
    This reverts commit 2571efcb3da9d0fc2040f4d35c317e2c3195605e.
    
    * sort glob output to improve reproducability of output json

--- bia-export/bia_export/website_models.py
+++ bia-export/bia_export/website_models.py
@@ -1,23 +1,51 @@
 from __future__ import annotations
-from pydantic import Field
+from pydantic import Field, BaseModel
 from typing import List, Optional
 from bia_shared_datamodels import bia_data_model
 
 
 class Study(bia_data_model.Study):
-    experimental_imaging_component: Optional[List[ExperimentalImagingDataset]] = Field(default_factory=list, description="""A dataset of that is associated with the study.""")
+    experimental_imaging_component: Optional[List[ExperimentalImagingDataset]] = Field(
+        default_factory=list,
+        description="""A dataset of that is associated with the study.""",
+    )
+
 
 class ExperimentalImagingDataset(bia_data_model.ExperimentalImagingDataset):
-    acquisition_process: list[bia_data_model.ImageAcquisition] = Field(
+    acquisition_process: list[ImageAcquisition] = Field(
         description="""Processes involved in the creation of the images and files in this dataset."""
     )
-    specimen_imaging_preparation_protocol: list[bia_data_model.SpecimenImagingPrepartionProtocol] = Field(
-        description="""Processes involved in the preprapartion of the samples for imaged."""
+    specimen_imaging_preparation_protocol: list[SpecimenImagingPrepartionProtocol] = (
+        Field(
+            description="""Processes involved in the preprapartion of the samples for imaged."""
+        )
     )
-    biological_entity: list[bia_data_model.BioSample] = Field(
+    biological_entity: list[BioSample] = Field(
         description="""The biological entity or entities that were imaged."""
     )
-    specimen_growth_protocol: Optional[list[bia_data_model.SpecimenGrowthProtocol]] = Field(
-        description="""Processes involved in the growth of the samples that were then imaged."""
+    specimen_growth_protocol: Optional[list[SpecimenGrowthProtocol]] = Field(
+        default_factory=list,
+        description="""Processes involved in the growth of the samples that were then imaged.""",
     )
 
+
+class DetailSection(BaseModel):
+    default_open: bool = Field()
+
+
+class ImageAcquisition(bia_data_model.ImageAcquisition, DetailSection):
+    pass
+
+
+class BioSample(bia_data_model.BioSample, DetailSection):
+    pass
+
+
+class SpecimenGrowthProtocol(bia_data_model.SpecimenGrowthProtocol, DetailSection):
+    pass
+
+
+class SpecimenImagingPrepartionProtocol(
+    bia_data_model.SpecimenImagingPrepartionProtocol, DetailSection
+):
+    pass
