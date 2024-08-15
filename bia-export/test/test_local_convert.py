5a34c13ab14e2cca41d790f7ecac89f96d86e3ba
Francois <fsherwood@ebi.ac.uk>
2024-08-15 13:05:59 +0100

    created export for image rembi metadata

--- bia-export/test/test_local_convert.py
+++ bia-export/test/test_local_convert.py
@@ -8,13 +8,27 @@ runner = CliRunner()
 
 
 
-def test_cli_export_website_studies(tmp_path):
+def test_cli_export_website_studies(tmp_path: Path):
     input_root_path = Path(__file__).parent.joinpath("input_data")
     expected_output = Path(__file__).parent.joinpath("output_data/bia_export.json")
     outfile = tmp_path.joinpath('bia_export.json').resolve()
 
     result = runner.invoke(app, ["website-study", "S-BIADTEST", "-o", outfile, "-r", input_root_path])
 
+
+
+    assert result.exit_code == 0
+    # Note this tests for exact equivance in files, i.e. order of fields and indentation matters
+    assert filecmp.cmp(expected_output, outfile, shallow=False)
+
+
+
+def test_cli_export_website_images(tmp_path: Path):
+    input_root_path = Path(__file__).parent.joinpath("input_data")
+    expected_output = Path(__file__).parent.joinpath("output_data/bia_image_export.json")
+    outfile = tmp_path.joinpath('bia_image_export.json').resolve()
+
+    result = runner.invoke(app, ["website-image", "S-BIADTEST", "-o", outfile, "-r", input_root_path])
 
 
     assert result.exit_code == 0
