ced3f7c2a39bc5446dad755cd68f288c8f25d415
kbab <32482596+kbab@users.noreply.github.com>
2024-08-21 10:26:32 +0100

    Tests for cli (#153)
    Create tests for CLI to ensure expected files with expected content written

--- bia-ingest-shared-models/test/test_file_reference.py
+++ bia-ingest-shared-models/test/test_file_reference.py
@@ -1,33 +1,16 @@
-""" Test FileReference creation
-   
-    Test FileReference creation separately from other shared models as it
-    now has a different pattern of creation from other artefacts i.e. 
-    it now needs a submitted dataset
+"""Test FileReference creation
+
+Test FileReference creation separately from other shared models as it
+now has a different pattern of creation from other artefacts i.e.
+it now needs a submitted dataset
 """
 
-from typing import Dict
-from pathlib import Path
-from unittest.mock import Mock
-import pytest
 from . import utils
 from bia_ingest_sm.conversion import (
-    experimental_imaging_dataset,
     file_reference,
 )
-from bia_ingest_sm.biostudies import requests, File
-from pydantic import TypeAdapter
+from bia_ingest_sm.biostudies import File
 
-# TODO: Mock requests.get correctly!!!
-def mock_request_get(flist_url: str) -> Dict[str, str]:
-    data_dir = Path(__file__).parent / "data"
-    path_to_load = data_dir / Path(flist_url).name
-    return_value = Mock()
-    return_value.status_code = 200
-    return_value.content = path_to_load.read_text()
-    return return_value
-
-
-requests.get = mock_request_get
 
 # Get second study component as dataset in submission
 datasets_in_submission = [
@@ -47,17 +30,19 @@ def test_get_file_reference_for_submission_dataset(test_submission, result_summa
         accession_id=test_submission.accno,
         submission_dataset=datasets_in_submission[0],
         files_in_file_list=files_in_filelist,
-        result_summary=result_summary
+        result_summary=result_summary,
     )
     assert created == expected
 
 
-def test_create_file_reference_for_study_component(test_submission, caplog, result_summary):
-
+def test_create_file_reference_for_study_component(
+    test_submission, caplog, result_summary, mock_request_get
+):
     expected = {datasets_in_submission[0].title_id: utils.get_test_file_reference()}
     created = file_reference.get_file_reference_by_dataset(
-        test_submission, datasets_in_submission=datasets_in_submission, result_summary=result_summary
-
+        test_submission,
+        datasets_in_submission=datasets_in_submission,
+        result_summary=result_summary,
     )
     assert created == expected
 
@@ -68,14 +53,18 @@ def test_create_file_reference_for_study_component(test_submission, caplog, resu
 def test_create_file_reference_for_study_component_when_no_matching_sc_in_file_list(
     test_submission, caplog, result_summary
 ):
-    """Test attempted creation of study FileReferences when study 
-        components in dataset do not match does in file_list
+    """Test attempted creation of study FileReferences when study
+    components in dataset do not match does in file_list
     """
 
     dataset = utils.get_test_experimental_imaging_dataset()[0]
     dataset.title_id = "Test name not in file list"
     created = file_reference.get_file_reference_by_dataset(
-        test_submission, datasets_in_submission=[dataset,], result_summary=result_summary
+        test_submission,
+        datasets_in_submission=[
+            dataset,
+        ],
+        result_summary=result_summary,
     )
 
     assert created is None
