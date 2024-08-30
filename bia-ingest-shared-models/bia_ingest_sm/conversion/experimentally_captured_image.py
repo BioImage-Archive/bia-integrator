643788ba2f60cce3272aa52431be33d644c419c4
Kola <kola@ebi.ac.uk>
2024-08-30 16:05:12 +0100

    Refactor code dealing with creation of ExperimentallyCapturedImage

--- bia-ingest-shared-models/bia_ingest_sm/conversion/experimentally_captured_image.py
+++ bia-ingest-shared-models/bia_ingest_sm/conversion/experimentally_captured_image.py
@@ -1,11 +1,16 @@
 import logging
 from uuid import UUID
-from typing import List
+from typing import List, Dict, Any
 from .utils import (
     dicts_to_api_models,
     dict_to_uuid,
     find_datasets_with_file_lists,
     get_bia_data_model_by_uuid,
+    filter_model_dictionary,
+)
+from ..image_utils.utils import (
+    get_image_extension,
+    extension_in_bioformats_single_file_formats_list,
 )
 from bia_ingest_sm.conversion.specimen import get_specimen_for_association
 from bia_ingest_sm.conversion.image_acquisition import get_image_acquisition
@@ -27,6 +32,18 @@ def get_all_experimentally_captured_images(
 ) -> List[bia_data_model.ExperimentallyCapturedImage]:
     """Return all experimentally captured images in all experimental imaging datasets"""
 
+    # TODO: Use of API will affect this! Retrieve if exists (Create otherwise?)
+    # TODO: Write function to get all objects of Type in accession_id subdir
+    image_acquisitions = None
+
+    # TODO: this is not really ingest - do we need result_summary?
+    if not image_acquisitions:
+        # Get all image acquisitions in study
+        image_acquisitions = get_image_acquisition(
+            submission, result_summary, persist_artefacts=persist_artefacts
+        )
+
+    # Experimental Imaging Datasets in study
     datasets = get_experimental_imaging_dataset(submission, result_summary)
     datasets_with_file_lists = find_datasets_with_file_lists(submission)
     model_dicts = []
@@ -51,42 +68,31 @@ def get_all_experimentally_captured_images(
             association["image_acquisition"]
             for association in dataset.attribute["associations"]
         ]
-        # TODO: Check if ImageAcquisitionMethod already exists. If not create
-        # TODO: Move this out of for loop. I.e. get all image acquisitions for study only once!
-        # without persisting?
+        # It would be nice to have a function like below ...
         # image_acquisitions = get_image_acquisition_by_title(submission.accno, image_acquisition_title)
-        image_acquisitions = None
-
-        if not image_acquisitions:
-            # Get all image acquisitions in study
-            image_acquisitions = get_image_acquisition(
-                submission, result_summary, persist_artefacts=persist_artefacts
-            )
-
         acquisition_process_uuid = [
             ia.uuid
             for ia in image_acquisitions
             if ia.title_id in image_acquisition_title
         ]
+
+        # TODO: revisit this once API up
         subject_uuid = get_specimen_for_association(
             submission, dataset.attribute["associations"][0]
         ).uuid
 
         for file_in_fl in files_in_fl:
-            # TODO: Need a way to filter relevant files
-            if not str(file_in_fl.path).endswith(".png"):
+            # Currently processing all single files bioformats can convert
+            extension = get_image_extension(str(file_in_fl.path))
+            if not extension_in_bioformats_single_file_formats_list(extension):
                 continue
 
-            model_dict = {
-                "path": str(file_in_fl.path),
-                "acquisition_process_uuid": acquisition_process_uuid,
-                "submission_dataset_uuid": dataset.uuid,
-                "subject_uuid": subject_uuid,
-            }
-            model_dict["uuid"] = dict_to_uuid(model_dict, list(model_dict.keys()))
-            model_dict.pop("path")
-            model_dict["attribute"] = {}
-            model_dict["version"] = 1
+            model_dict = prepare_experimentally_captured_image_dict(
+                file_paths=str(file_in_fl.path),
+                acquisition_process_uuid=acquisition_process_uuid,
+                dataset_uuid=dataset.uuid,
+                subject_uuid=subject_uuid,
+            )
             model_dicts.append(model_dict)
     experimentally_captured_images = dicts_to_api_models(
         model_dicts,
@@ -114,8 +120,11 @@ def get_experimentally_captured_image(
         association["image_acquisition"]
         for association in dataset.attribute["associations"]
     ]
+    # TODO: Use of API will affect this! Retrieve if exists (Create otherwise?)
+    # TODO: Write function to get all objects of Type in accession_id subdir
     image_acquisitions = None
 
+    # TODO: this is not really ingest - do we need result_summary?
     result_summary = {submission.accno: {}}
     if not image_acquisitions:
         # Get all image acquisitions in study
@@ -130,164 +139,48 @@ def get_experimentally_captured_image(
         submission, dataset.attribute["associations"][0]
     ).uuid
 
-    model_dict = {
-        # "file_reference_uuid": ",".join([str(f) for f in file_reference_uuids]),
-        "path": ",".join([fp for fp in file_paths]),
-        "acquisition_process_uuid": acquisition_process_uuid,
-        "submission_dataset_uuid": dataset.uuid,
-        "subject_uuid": subject_uuid,
-    }
-    model_dict["uuid"] = dict_to_uuid(model_dict, list(model_dict.keys()))
-    model_dict.pop("path")
-    # model_dict.pop("file_reference_uuid")
-    model_dict["attribute"] = {}
-    model_dict["version"] = 1
-
+    model_dict = prepare_experimentally_captured_image_dict(
+        file_paths=",".join([fp for fp in file_paths]),
+        acquisition_process_uuid=acquisition_process_uuid,
+        dataset_uuid=dataset.uuid,
+        subject_uuid=subject_uuid,
+    )
     return bia_data_model.ExperimentallyCapturedImage.model_validate(model_dict)
 
 
-#    submission: Submission, result_summary: dict, persist_artefacts=False
-# ) -> List[bia_data_model.ExperimentalImagingDataset]:
-#    """
-#    Map biostudies.Submission study components to bia_data_model.ExperimentalImagingDataset
-#    """
-#    study_components = find_sections_recursive(
-#        submission.section,
-#        [
-#            "Study Component",
-#        ],
-#        [],
-#    )
-#    analysis_method_dict = get_image_analysis_method(submission, result_summary)
-#
-#    experimental_imaging_dataset = []
-#    for section in study_components:
-#        attr_dict = attributes_to_dict(section.attributes)
-#        key_mapping = [
-#            (
-#                "image_analysis",
-#                "Image analysis",
-#                None,
-#            ),
-#            (
-#                "image_correlation",
-#                "Image correlation",
-#                None,
-#            ),
-#            (
-#                "biosample",
-#                "Biosample",
-#                None,
-#            ),
-#            (
-#                "image_acquisition",
-#                "Image acquisition",
-#                None,
-#            ),
-#            (
-#                "specimen",
-#                "Specimen",
-#                None,
-#            ),
-#        ]
-#        associations = get_generic_section_as_list(
-#            section,
-#            [
-#                "Associations",
-#            ],
-#            key_mapping,
-#        )
-#
-#        analysis_method_list = []
-#        correlation_method_list = []
-#
-#        # TODO: move this to main CLI code to make object generation more independent
-#        if len(associations) > 0:
-#            # Image Analysis Method
-#            analysis_methods_from_associations = [
-#                a.get("image_analysis") for a in associations
-#            ]
-#            for analysis_method in analysis_method_dict.values():
-#                if (
-#                    analysis_method.protocol_description
-#                    in analysis_methods_from_associations
-#                ):
-#                    analysis_method_list.append(analysis_method)
-#
-#        section_name = attr_dict["Name"]
-#        model_dict = {
-#            "title_id": section_name,
-#            "description": attr_dict["Description"],
-#            "submitted_in_study_uuid": study_conversion.get_study_uuid(submission),
-#            "analysis_method": analysis_method_list,
-#            "correlation_method": correlation_method_list,
-#            "example_image_uri": [],
-#            "version": 1,
-#            "attribute": {"associations": associations},
-#        }
-#        model_dict["uuid"] = generate_experimental_imaging_dataset_uuid(model_dict)
-#
-#        model_dict = filter_model_dictionary(
-#            model_dict, bia_data_model.ExperimentalImagingDataset
-#        )
-#
-#
-#        try:
-#            experimental_imaging_dataset.append(
-#            bia_data_model.ExperimentalImagingDataset.model_validate(model_dict)
-#        )
-#        except(ValidationError):
-#            log_failed_model_creation(bia_data_model.ExperimentalImagingDataset, result_summary)
-#
-#
-#    logger.info(
-#        f"Ingesting: {submission.accno}. Created bia_data_model.ExperimentalImagingDataset. Count: {len(experimental_imaging_dataset)}"
-#    )
-#
-#    if persist_artefacts and experimental_imaging_dataset:
-#        persist(
-#            experimental_imaging_dataset,
-#            "experimental_imaging_datasets",
-#            submission.accno,
-#        )
-#
-#    return experimental_imaging_dataset
-#
-#
-# def get_image_analysis_method(
-#    submission: Submission,
-#    result_summary: dict
-# ) -> Dict[str, semantic_models.ImageAnalysisMethod]:
-#    key_mapping = [
-#        (
-#            "protocol_description",
-#            "Title",
-#            None,
-#        ),
-#        (
-#            "features_analysed",
-#            "Image analysis overview",
-#            None,
-#        ),
-#    ]
-#
-#    return get_generic_section_as_dict(
-#        submission,
-#        [
-#            "Image analysis",
-#        ],
-#        key_mapping,
-#        semantic_models.ImageAnalysisMethod,
-#        result_summary[submission.accno],
-#    )
-#
-#
-# def generate_experimental_imaging_dataset_uuid(
-#    experimental_imaging_dataset_dict: Dict[str, Any],
-# ) -> str:
-#    # TODO: Add 'description' to computation of uuid (Maybe accno?)
-#    attributes_to_consider = [
-#        "title_id",
-#        "submitted_in_study_uuid",
-#    ]
-#    return dict_to_uuid(experimental_imaging_dataset_dict, attributes_to_consider)
+def prepare_experimentally_captured_image_dict(
+    dataset_uuid: UUID,
+    file_paths: str,
+    acquisition_process_uuid: List[UUID],
+    subject_uuid: UUID,
+    attribute: dict = {},
+    version: int = 1,
+):
+    model_dict = {
+        # TODO: Are file_reference uuids a better choice here?
+        # "file_reference_uuid": ",".join([str(f) for f in file_reference_uuids]),
+        "path": file_paths,
+        "acquisition_process_uuid": acquisition_process_uuid,
+        "submission_dataset_uuid": dataset_uuid,
+        "subject_uuid": subject_uuid,
+        "attribute": attribute,
+        "version": version,
+    }
+    model_dict["uuid"] = generate_experimentally_captured_image_uuid(model_dict)
+    return filter_model_dictionary(
+        model_dict, bia_data_model.ExperimentallyCapturedImage
+    )
+
+
+def generate_experimentally_captured_image_uuid(
+    experimentally_captured_image_dict: Dict[str, Any],
+) -> str:
+    attributes_to_consider = [
+        # TODO: Are file_reference uuids a better choice here?
+        # "file_reference_uuid": ",".join([str(f) for f in file_reference_uuids]),
+        "path",
+        "acquisition_process_uuid",
+        "submission_dataset_uuid",
+        "subject_uuid",
+    ]
+    return dict_to_uuid(experimentally_captured_image_dict, attributes_to_consider)
---
+++ bia-ingest-shared-models/bia_ingest_sm/image_utils/resources/bioformats_curated_file_formats_readme.txt
@@ -0,0 +1,14 @@
+Curated files last updated 21/05/2024
+
+Notes on creating the curated files:
+
+1. Copy supported formats from https://bio-formats.readthedocs.io/en/stable/supported-formats.html
+2. Paste into spreadsheet or text editor and get all extensions
+3. Ensure extensions are unique and sorted
+4. Manually curate into 'bioformats_curated_single_file_formats.txt' Which have 1-2-1 conversion with bioformats2raw and bioformats_curated_other_file_formats.txt which require more input for conversion (e.g. pattern files)
+
+The above steps can be accomplished in a browser developer console using the following js snippet (thanks to LA):
+
+[...new Set(Array.from(document.getElementsByTagName("tbody")[0].querySelectorAll("td:nth-child(2)")).map(el => el.innerText.split(",")).flat().filter(extension => extension.length))].sort()
+
+TODO: write python version of js snippet e.g. using selinium
