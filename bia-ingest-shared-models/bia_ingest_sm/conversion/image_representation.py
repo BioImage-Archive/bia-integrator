74747098574bc5cab1e74409705181c907abb4e5
Kola <kola@ebi.ac.uk>
2024-09-03 10:45:34 +0100

    Save WIP - Add artefacts for creating and testing image representations

---
+++ bia-ingest-shared-models/bia_ingest_sm/conversion/image_representation.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+import logging
+from typing import List
+from uuid import UUID
+from .utils import dict_to_uuid, get_bia_data_model_by_uuid
+from ..image_utils import image_utils
+from .experimentally_captured_image import get_experimentally_captured_image
+from ..config import settings
+from ..biostudies import (
+    Submission,
+)
+from bia_shared_datamodels import bia_data_model
+
+
+logger = logging.getLogger("__main__." + __name__)
+
+
+def image_representation_from_zarr(
+    submission: Submission, file_reference_uuids: List[UUID], zarr_location: str
+) -> bia_data_model.ImageRepresentation:
+    """Create ImageRepresentation for specified FileReference(s) and zarr"""
+
+    # TODO: this should be replaced by API client and we would not need
+    # accession_id
+    file_references = [
+        get_bia_data_model_by_uuid(uuid, bia_data_model.FileReference, submission.accno)
+        for uuid in file_reference_uuids
+    ]
+    file_uris = [file_reference.uri for file_reference in file_references]
+    experimentally_captured_image = get_experimentally_captured_image(
+        submission=submission,
+        dataset_uuid=file_references[0].submission_dataset_uuid,
+        file_paths=[fr.file_path for fr in file_references],
+    )
+
+    model_dict = {
+        "image_format": "",
+        "file_uri": file_uris,
+        "original_file_reference_uuid": file_reference_uuids,
+        "representation_of_uuid": experimentally_captured_image.uuid,
+        "total_size_in_bytes": image_utils.get_total_zarr_size(zarr_location),
+        "attribute": {},
+        "version": 1,
+    }
+    model_dict["uuid"] = dict_to_uuid(model_dict, list(model_dict.keys()))
+
+    return bia_data_model.ImageRepresentation.model_validate(model_dict)
+    # return dicts_to_api_models([model_dict,], bia_data_model.ImageRepresentation )[0]
+
+
+def _get_file_reference_from_disk(
+    uuid: UUID, accession_id: str
+) -> bia_data_model.FileReference:
+    """Get FileRefence from expected path on disk"""
+
+    input_path = (
+        Path(settings.bia_data_dir) / "file_references" / accession_id / f"{uuid}.json"
+    )
+    return bia_data_model.FileReference.model_validate_json(input_path.read_text())
---
+++ bia-ingest-shared-models/test/test_image_representation.py
@@ -0,0 +1,62 @@
+"""Test ImageRepresentation creation"""
+
+from pathlib import Path
+from . import utils
+from bia_shared_datamodels import bia_data_model
+from bia_ingest_sm.conversion import (
+    image_representation,
+)
+
+experimentally_captured_image_uuid = utils.get_test_experimentally_captured_image()[
+    0
+].uuid
+test_file_reference = utils.get_test_file_reference(
+    ["file_list_study_component_1.json"]
+)[0]
+# test_zarr_location = f"file://{Path(__file__).parent / 'data' / 'test_image_representations' / 'study_component1' / 'im06.zarr'}"
+test_zarr_location = f"{Path(__file__).parent / 'data' / 'test_image_representations' / 'study_component1' / 'im06.zarr'}"
+
+
+def test_get_create_zarr_representation_of_single_image(
+    test_submission, result_summary, monkeypatch
+):
+    model_dict = {
+        "image_format": "",
+        "file_uri": [
+            test_file_reference.uri,
+        ],
+        "original_file_reference_uuid": [test_file_reference.uuid],
+        "representation_of_uuid": experimentally_captured_image_uuid,
+        "total_size_in_bytes": 60352,
+        # "physical_size_x": 0,
+        # "physical_size_y": 0,
+        # "physical_size_z": 0,
+        # "size_x": 100,
+        # "size_y": 80,
+        # "size_z": 1,
+        # "size_t": 1,
+        "attribute": {},
+        "version": 1,
+    }
+    model_dict["uuid"] = utils.dict_to_uuid(model_dict, list(model_dict.keys()))
+    expected = bia_data_model.ImageRepresentation.model_validate(model_dict)
+
+    test_file_reference_uuids = [
+        test_file_reference.uuid,
+    ]
+
+    def mock_return_file_reference(input_path):
+        input_path = str(input_path)
+        if "file_reference" in input_path:
+            return test_file_reference.model_dump_json()
+        elif "experimental_imaging_dataset" in input_path:
+            return utils.get_test_experimental_imaging_dataset()[0].model_dump_json()
+
+    monkeypatch.setattr(
+        image_representation.Path, "read_text", mock_return_file_reference
+    )
+
+    created = image_representation.image_representation_from_zarr(
+        test_submission, test_file_reference_uuids, zarr_location=test_zarr_location
+    )
+    assert created == expected
