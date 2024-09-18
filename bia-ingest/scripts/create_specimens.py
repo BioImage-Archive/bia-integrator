7f9816bb8be1c798c4be8bef9ea3533f67ecc938
Kola <kola@ebi.ac.uk>
2024-09-18 14:13:29 +0100

    Remove unneeded function

--- bia-ingest/scripts/create_specimens.py
+++ bia-ingest/scripts/create_specimens.py
@@ -24,15 +24,6 @@ def create_and_persist_specimens(accession_id):
     print(result_summary)
 
 
-# From https://stackoverflow.com/questions/1094841/get-human-readable-version-of-file-size
-def sizeof_fmt(num, suffix="B"):
-    for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
-        if abs(num) < 1024.0:
-            return f"{num:3.1f}{unit}{suffix}"
-        num /= 1024.0
-    return f"{num:.1f}Yi{suffix}"
-
-
 if __name__ == "__main__":
     accession_id = sys.argv[1]
     create_and_persist_specimens(accession_id)
