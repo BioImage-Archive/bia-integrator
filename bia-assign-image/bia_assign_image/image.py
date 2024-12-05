9410fb885f8c6163c380b6aedc3f954245e4a039
Kola <kola@ebi.ac.uk>
2024-12-05 00:02:31 +0000

    Save WIP at point of cli creation

--- bia-assign-image/bia_assign_image/image.py
+++ bia-assign-image/bia_assign_image/image.py
@@ -1,6 +1,14 @@
+from uuid import UUID
+from typing import List
 from bia_shared_datamodels import bia_data_model, uuid_creation
 
-def get_image(submission_dataset_uuid, creation_process_uuid, original_file_reference_uuid):
+# TODO: Explore just passing original_file_reference_uuid then deriving
+#       other UUIDs from this.
+def get_image(
+        submission_dataset_uuid: UUID,
+        creation_process_uuid: UUID,
+        original_file_reference_uuid: List[UUID,],
+    ) -> bia_data_model.Image:
     image_dict = {
         "version": 0,
         "submission_dataset_uuid": submission_dataset_uuid,
@@ -10,5 +18,6 @@ def get_image(submission_dataset_uuid, creation_process_uuid, original_file_refe
     image_dict["uuid"] = uuid_creation.create_image_uuid(
         original_file_reference_uuid
     )
+    # TODO: Add attributes from each file reference
 
     return bia_data_model.Image.model_validate(image_dict)
