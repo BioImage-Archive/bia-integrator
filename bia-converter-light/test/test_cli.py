06c7a528967b6e1fde94b41a68ee5afc23e8c3dc
Kola <kola@ebi.ac.uk>
2024-12-24 17:23:33 +0000

    Save WIP during refactoring

--- bia-converter-light/test/test_cli.py
+++ bia-converter-light/test/test_cli.py
@@ -1,20 +1,87 @@
-from pathlib import Path
 import pytest
 from typer.testing import CliRunner
+from unittest.mock import MagicMock
 
+from bia_shared_datamodels import bia_data_model
 from bia_test_data.mock_objects.mock_object_constants import accession_id
 from bia_test_data.mock_objects import (
     mock_image_representation,
     mock_image,
     mock_dataset,
 )
-from bia_ingest.persistence_strategy import persistence_strategy_factory
-from bia_converter_light.cli import app, settings
+from bia_ingest.persistence_strategy import (
+    persistence_strategy_factory,
+    PersistenceStrategy,
+)
+from bia_converter_light import cli
 
 
 @pytest.fixture
 def runner() -> CliRunner:
     return CliRunner(mix_stderr=False)
+
+
+@pytest.fixture
+def output_dir_base(tmpdir):
+    return tmpdir
+
+
+@pytest.fixture
+def persister(output_dir_base) -> PersistenceStrategy:
+    persister = persistence_strategy_factory(
+        persistence_mode="disk",
+        output_dir_base=str(output_dir_base),
+        accession_id=accession_id,
+    )
+    return persister
+
+
+@pytest.fixture
+def mock_api_client(monkeypatch, persister):
+    """Mock api_client functions for getting and saving model objects used in test"""
+
+    def mock_get_image(uuid):
+        return persister.fetch_by_uuid(
+            [
+                uuid,
+            ],
+            bia_data_model.Image,
+        )[0]
+
+    def mock_get_image_representation(uuid):
+        return persister.fetch_by_uuid(
+            [
+                uuid,
+            ],
+            bia_data_model.ImageRepresentation,
+        )[0]
+
+    def mock_get_dataset(uuid):
+        return persister.fetch_by_uuid(
+            [
+                uuid,
+            ],
+            bia_data_model.Dataset,
+        )[0]
+
+    def mock_post_object(obj):
+        persister.persist(
+            [
+                obj,
+            ]
+        )
+
+    mock_api_client_object = MagicMock()
+    mock_api_client_object.get_image_representation = mock_get_image_representation
+    mock_api_client_object.get_dataset = mock_get_dataset
+    mock_api_client_object.get_image = mock_get_image
+    mock_api_client_object.post_dataset = mock_post_object
+    monkeypatch.setattr(
+        cli,
+        "api_client",
+        mock_api_client_object,
+    )
+    return mock_api_client_object
 
 
 def test_cli_convert_image(runner):
@@ -23,7 +90,7 @@ def test_cli_convert_image(runner):
     # )
 
     result = runner.invoke(
-        app,
+        cli.app,
         [
             "convert-image",
             # "S-BIADTEST",
@@ -37,13 +104,9 @@ def test_cli_convert_image(runner):
     assert result.exit_code == 0
 
 
-def test_cli_update_example_image_uri_for_dataset(runner, tmpdir, monkeypatch):
-    output_dir_base = tmpdir
-    persister = persistence_strategy_factory(
-        persistence_mode="disk",
-        output_dir_base=str(output_dir_base),
-        accession_id=accession_id,
-    )
+def test_cli_update_example_image_uri_for_dataset(
+    runner, mock_api_client, output_dir_base, persister, monkeypatch
+):
     image_representation = (
         mock_image_representation.get_image_representation_of_static_display()
     )
@@ -67,28 +130,24 @@ def test_cli_update_example_image_uri_for_dataset(runner, tmpdir, monkeypatch):
         ]
     )
 
-    created_image_path = Path(output_dir_base) / "image"
-    if created_image_path.exists():
-        for p in created_image_path.rglob("*.json"):
-            p.unlink()
-        for d in created_image_path.rglob("*"):
-            if d.is_dir():
-                d.rmdir()
-        created_image_path.rmdir()
-
-    monkeypatch.setattr(settings, "bia_data_dir", str(output_dir_base))
+    monkeypatch.setattr(cli.settings, "bia_data_dir", str(output_dir_base))
+    monkeypatch.setattr(cli, "api_client", mock_api_client)
     result = runner.invoke(
-        app,
+        cli.app,
         [
             "update-example-image-uri-for-dataset",
-            accession_id,
-            str(dataset.uuid),
             str(image_representation.uuid),
             # "--help"
         ],
     )
 
     assert result.exit_code == 0
-    modified_dataset = persister.fetch_by_uuid(dataset.uuid, dataset.__name__)
+    # cli.update_example_image_uri_for_dataset(image_representation.uuid)
+    modified_dataset = persister.fetch_by_uuid(
+        [
+            dataset.uuid,
+        ],
+        bia_data_model.Dataset,
+    )[0]
     assert dataset.example_image_uri == []
     assert modified_dataset.example_image_uri == image_representation.file_uri
