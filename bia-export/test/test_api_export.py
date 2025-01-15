74300fc30a586c251108f4a29318243ac7ae521f
Francois <fsherwood@ebi.ac.uk>
2025-01-15 13:47:29 +0000

    added additional tests for api cli commands

--- bia-export/test/test_api_export.py
+++ bia-export/test/test_api_export.py
@@ -6,7 +6,7 @@ import json
 runner = CliRunner()
 
 
-def test_cli_export_website_studies_api(tmp_path: Path, data_in_api):
+def test_cli_export_website_studies(tmp_path: Path, data_in_api):
     expected_output = Path(__file__).parent.joinpath(
         "output_data/bia-study-metadata.json"
     )
@@ -21,6 +21,47 @@ def test_cli_export_website_studies_api(tmp_path: Path, data_in_api):
             "-o",
             outfile,
         ],
+    )
+
+    assert result.exit_code == 0
+
+    with open(outfile, "r") as f:
+        json_result = json.load(f)
+
+    with open(expected_output) as f:
+        json_expected = json.load(f)
+
+    assert json_result == json_expected
+
+
+def test_cli_export_website_images(tmp_path: Path):
+    expected_output = Path(__file__).parent.joinpath(
+        "output_data/bia-image-export.json"
+    )
+    outfile = tmp_path.joinpath("bia-image-export.json").resolve()
+
+    result = runner.invoke(app, ["website", "image", "S-BIADTEST", "-o", outfile])
+
+    assert result.exit_code == 0
+
+    with open(outfile, "r") as f:
+        json_result = json.load(f)
+
+    with open(expected_output) as f:
+        json_expected = json.load(f)
+
+    assert json_result == json_expected
+
+
+def test_cli_export_dataset_for_website_images(tmp_path: Path):
+    expected_output = Path(__file__).parent.joinpath(
+        "output_data/bia-dataset-metadata-for-images.json"
+    )
+    outfile = tmp_path.joinpath("bia-dataset-metadata.json").resolve()
+
+    result = runner.invoke(
+        app,
+        ["website", "image-dataset", "S-BIADTEST", "-o", outfile],
     )
 
     assert result.exit_code == 0
