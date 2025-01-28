45398359e4c67aa4b697e68ce7dbd625fe9f7ccf
Francois <fsherwood@ebi.ac.uk>
2025-01-28 11:25:23 +0000

    fixes to glob call and initializing chain length count

--- bia-test-data/bia_test_data/data_to_api.py
+++ bia-test-data/bia_test_data/data_to_api.py
@@ -41,9 +41,9 @@ def get_object_creation_client(
     return private_api
 
 
-
-
-def calculate_dependency_chain_length(object_by_type: dict[str, dict[str, dict]]) -> dict[str, int]:
+def calculate_dependency_chain_length(
+    object_by_type: dict[str, dict[str, dict]]
+) -> dict[str, int]:
     # Images need their creation process to be added to the api before they can be added
     # Some creation processes need images to be added to the api before they can be added
     # dependecy_chain_length is the maximum number of times a change in type of object being added to the api has to occur before this object can be added to the api.
@@ -53,27 +53,49 @@ def calculate_dependency_chain_length(object_by_type: dict[str, dict[str, dict]]
 
     for uuid, creation_process in object_by_type["CreationProcess"].items():
         if uuid not in dependecy_chain_length.keys():
-            creation_process_dependency_chain_length(creation_process, dependecy_chain_length, object_by_type["Image"], object_by_type["CreationProcess"])
-        
+            creation_process_dependency_chain_length(
+                creation_process,
+                dependecy_chain_length,
+                object_by_type["Image"],
+                object_by_type["CreationProcess"],
+            )
+
     for uuid, image in object_by_type["Image"].items():
         if uuid not in dependecy_chain_length.keys():
-            image_dependency_chain_length(image, dependecy_chain_length, object_by_type["Image"], object_by_type["CreationProcess"])
+            image_dependency_chain_length(
+                image,
+                dependecy_chain_length,
+                object_by_type["Image"],
+                object_by_type["CreationProcess"],
+            )
     return dependecy_chain_length
 
 
-
-def creation_process_dependency_chain_length(creation_process: dict, dependency_chain_length: dict, images: dict[str, dict], creation_processes: dict[str, dict]) -> int:
-    uuid = creation_process['uuid']
+def creation_process_dependency_chain_length(
+    creation_process: dict,
+    dependency_chain_length: dict,
+    images: dict[str, dict],
+    creation_processes: dict[str, dict],
+) -> int:
+    uuid = creation_process["uuid"]
 
     # Creation processes either do not depend on any images (in which case dependency chain length is 0)
     # Or they need to be created after the last image they depend on gets created (so max(image dependency chain length) + 1)
     if uuid not in dependency_chain_length.keys():
-        if not creation_process['input_image_uuid'] or len(creation_process['input_image_uuid']) == 0:
+        if (
+            not creation_process["input_image_uuid"]
+            or len(creation_process["input_image_uuid"]) == 0
+        ):
             dependency_chain_length[uuid] = 0
         else:
             max_length = 0
-            for image_uuid in creation_process['input_image_uuid']:
-                img_chain_length = image_dependency_chain_length(images[image_uuid], dependency_chain_length, images, creation_processes)
+            for image_uuid in creation_process["input_image_uuid"]:
+                img_chain_length = image_dependency_chain_length(
+                    images[image_uuid],
+                    dependency_chain_length,
+                    images,
+                    creation_processes,
+                )
                 if img_chain_length > max_length:
                     max_length = img_chain_length
             dependency_chain_length[uuid] = max_length + 1
@@ -81,33 +103,48 @@ def creation_process_dependency_chain_length(creation_process: dict, dependency_
     return dependency_chain_length[uuid]
 
 
-def image_dependency_chain_length(image: dict, dependency_chain_length: dict, images: dict[str, dict], creation_processes: dict[str, dict]) -> int:
-    uuid = image['uuid']
+def image_dependency_chain_length(
+    image: dict,
+    dependency_chain_length: dict,
+    images: dict[str, dict],
+    creation_processes: dict[str, dict],
+) -> int:
+    uuid = image["uuid"]
 
     # Images depend on at least 1 creation process, so always have a dependency chain length of: creation process dependcy chain length + 1
     if uuid not in dependency_chain_length.keys():
         if image["creation_process_uuid"] in dependency_chain_length.keys():
-            dependency_chain_length[uuid] = dependency_chain_length[image["creation_process_uuid"]] + 1
+            dependency_chain_length[uuid] = (
+                dependency_chain_length[image["creation_process_uuid"]] + 1
+            )
         else:
             cp_uuid = image["creation_process_uuid"]
-            cp_chain_length = creation_process_dependency_chain_length(creation_processes[cp_uuid], dependency_chain_length, images, creation_processes)
+            cp_chain_length = creation_process_dependency_chain_length(
+                creation_processes[cp_uuid],
+                dependency_chain_length,
+                images,
+                creation_processes,
+            )
             dependency_chain_length[uuid] = cp_chain_length + 1
-    
+
     return dependency_chain_length[uuid]
 
 
-
-def add_simple_dependecy_object(object_type: str, object_by_type: dict[str, dict], ordered_object_list: list) -> None:
+def add_simple_dependecy_object(
+    object_type: str, object_by_type: dict[str, dict], ordered_object_list: list
+) -> None:
     if object_type in object_by_type.keys():
         for study in object_by_type[object_type].values():
             ordered_object_list.append(study)
 
 
-def add_creation_processes_and_images(object_by_type: dict[str,dict], ordered_object_list: list[dict]):
+def add_creation_processes_and_images(
+    object_by_type: dict[str, dict], ordered_object_list: list[dict]
+):
     if "CreationProcess" in object_by_type.keys() and "Image" in object_by_type.keys():
 
         dependecy_chain_length = calculate_dependency_chain_length(object_by_type)
-    
+
         uuid_order: dict[int, list[str]] = {}
         max_chain_length = 0
         for uuid, chain_length in dependecy_chain_length.items():
@@ -117,6 +154,7 @@ def add_creation_processes_and_images(object_by_type: dict[str,dict], ordered_ob
             if chain_length > max_chain_length:
                 max_chain_length = chain_length
 
+        chain_length = 0
         while chain_length <= max_chain_length:
             for uuid in uuid_order[chain_length]:
                 if uuid in object_by_type["CreationProcess"].keys():
@@ -126,11 +164,11 @@ def add_creation_processes_and_images(object_by_type: dict[str,dict], ordered_ob
             chain_length += 1
 
     elif "CreationProcess" in object_by_type.keys():
-        add_simple_dependecy_object("CreationProcess", object_by_type, ordered_object_list)
+        add_simple_dependecy_object(
+            "CreationProcess", object_by_type, ordered_object_list
+        )
     elif "Image" in object_by_type.keys():
         add_simple_dependecy_object("Image", object_by_type, ordered_object_list)
-
-
 
 
 def order_object_for_api(object_list: list[dict]):
@@ -141,7 +179,7 @@ def order_object_for_api(object_list: list[dict]):
         object_type = object["model"]["type_name"]
         if object_type not in object_by_type:
             object_by_type[object_type] = {}
-        object_by_type[object_type][object['uuid']] = object
+        object_by_type[object_type][object["uuid"]] = object
 
     ordered_object_list = []
 
@@ -150,16 +188,21 @@ def order_object_for_api(object_list: list[dict]):
     add_simple_dependecy_object("FileReference", object_by_type, ordered_object_list)
     add_simple_dependecy_object("Protocol", object_by_type, ordered_object_list)
     add_simple_dependecy_object("BioSample", object_by_type, ordered_object_list)
-    add_simple_dependecy_object("SpecimenImagingPreparationProtocol", object_by_type, ordered_object_list)
+    add_simple_dependecy_object(
+        "SpecimenImagingPreparationProtocol", object_by_type, ordered_object_list
+    )
     add_simple_dependecy_object("Specimen", object_by_type, ordered_object_list)
-    add_simple_dependecy_object("ImageAcquisitionProtocol", object_by_type, ordered_object_list)
+    add_simple_dependecy_object(
+        "ImageAcquisitionProtocol", object_by_type, ordered_object_list
+    )
     add_simple_dependecy_object("AnnotationMethod", object_by_type, ordered_object_list)
     # Only images and creation process can depend on one another
     add_creation_processes_and_images(object_by_type, ordered_object_list)
-    add_simple_dependecy_object("ImageRepresentation", object_by_type, ordered_object_list)
+    add_simple_dependecy_object(
+        "ImageRepresentation", object_by_type, ordered_object_list
+    )
 
     return ordered_object_list
-
 
 
 def add_objects_to_api(api_base_url, object_list: list[dict]):
