cf90db89b28bf5f67603be5acd458a3e528aedb8
Kola <kola@ebi.ac.uk>
2025-05-07 15:21:51 +0100

    Make image representations work with new models

--- bia-assign-image/tests/test_image_representation.py
+++ bia-assign-image/tests/test_image_representation.py
@@ -1,51 +1,64 @@
 """Test ImageRepresentation creation"""
 
+from uuid import UUID
+from pathlib import Path
 import pytest
-from bia_test_data.mock_objects import (
-    mock_file_reference,
-    mock_image_representation,
-    mock_image,
-)
-from bia_test_data.mock_objects.mock_object_constants import accession_id
+from bia_shared_datamodels import bia_data_model, uuid_creation
 from bia_assign_image import image_representation
+from .conftest import get_expected_object
+
+# TODO: Create accession ID and artefacts specific to this test
+ACCESSION_ID = "S-BIAD-TEST-ASSIGN-IMAGE"
+EXPECTED_OBJECT_BASE_PATH = Path(__file__).parent / "test_data"
+INPUT_DATA_BASE_PATH = Path(__file__).parent / "input_data"
 
 
-@pytest.mark.parametrize(
-    ("image_representation_use_type", "mock_creation_function"),
-    (
-        (
-            "uploaded_by_submitter",
-            mock_image_representation.get_image_representation_of_uploaded_by_submitter,
-        ),
-        (
-            "thumbnail",
-            mock_image_representation.get_image_representation_of_thumbnail,
-        ),
-        (
-            "static_display",
-            mock_image_representation.get_image_representation_of_static_display,
-        ),
-        (
-            "interactive_display",
-            mock_image_representation.get_image_representation_of_interactive_display,
-        ),
-    ),
-)
-def test_create_representation_of_single_image(
-    image_representation_use_type,
-    mock_creation_function,
-):
-    if image_representation_use_type == "uploaded_by_submitter":
-        expected = mock_creation_function(with_file_uri_set=True)
-    else:
-        expected = mock_creation_function(with_file_uri_set=False)
+@pytest.fixture
+def study_uuid() -> UUID:
+    return uuid_creation.create_study_uuid(ACCESSION_ID)
 
-    file_references = mock_file_reference.get_file_reference()
-    created = image_representation.get_image_representation(
-        accession_id=accession_id,
-        file_references=file_references[:1],
-        image=mock_image.get_image_with_one_file_reference(),
-        use_type=image_representation_use_type,
+
+@pytest.fixture
+def file_reference():
+    uuid = "13fd33bf-89b1-4080-9ab1-b8d804e7850b"
+    return get_expected_object(
+        # TODO: resolve issue with version then use INPUT_DATA_BASE_PATH
+        # INPUT_DATA_BASE_PATH, "FileReference", ACCESSION_ID, uuid
+        EXPECTED_OBJECT_BASE_PATH,
+        "FileReference",
+        ACCESSION_ID,
+        uuid,
     )
 
-    assert created == expected
+
+@pytest.fixture
+def expected_image_representation() -> bia_data_model.ImageRepresentation:
+    uuid = "61259a4d-4844-4400-a7f7-c3ccaadb51d8"
+    return get_expected_object(
+        EXPECTED_OBJECT_BASE_PATH, "ImageRepresentation", ACCESSION_ID, uuid
+    )
+
+
+@pytest.fixture
+def image() -> bia_data_model.Image:
+    image_uuid = "aca07c38-9575-4f4e-b2cc-018b2a3e50b1"
+    return get_expected_object(
+        EXPECTED_OBJECT_BASE_PATH, "Image", ACCESSION_ID, image_uuid
+    )
+
+
+def test_create_representation_of_single_image(
+    study_uuid,
+    image,
+    expected_image_representation,
+    file_reference,
+):
+    created = image_representation.get_image_representation(
+        study_uuid=study_uuid,
+        file_references=[
+            file_reference,
+        ],
+        image=image,
+    )
+
+    assert created == expected_image_representation
