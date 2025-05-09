73ac4fcc6992f9defc6e61193a9245840a88a61f
Kola <kola@ebi.ac.uk>
2025-05-09 15:50:49 +0100

    Save WIP at point mapping Image and ImageRepresentation to 2025/04 models work

--- bia-assign-image/scripts/map_image_related_artefacts_to_2025_04_models.py
+++ bia-assign-image/scripts/map_image_related_artefacts_to_2025_04_models.py
@@ -1,36 +1,70 @@
-from bia_shared_datamodels import bia_data_model, semantic_models, uuid_creation
-import re
+from bia_shared_datamodels import bia_data_model, uuid_creation
+import copy
+from bia_assign_image.cli import assign
+from bia_assign_image.api_client import get_api_client
+
+
+def map_image_to_2025_04_model(
+    old_image_dict: dict,
+    accession_id: str,
+    file_reference_uuids_2025_04: list,
+    dataset_uuid_2025_04: str,
+    api_target,
+) -> bia_data_model.Image:
+    image_dict = copy.deepcopy(old_image_dict)
+    image_dict["model"] = {"type_name": "Image", "version": 2}
+
+    # Make file_reference_uuids a list of strings (because of quirky
+    # way the typer cli arguments are implemented in bia_assign_image.cli.assign)
+    file_reference_uuids = [
+        " ".join([str(f) for f in file_reference_uuids_2025_04]),
+    ]
+
+    image_uuid = assign(accession_id, file_reference_uuids, api_target)
+
+    api_client = get_api_client(api_target)
+    mapped_image = api_client.get_image(image_uuid)
+    return bia_data_model.Image.model_validate(mapped_image.to_dict())
 
 
 def map_image_representation_to_2025_04_model(
-    image_representation_dict: dict, image_2025_04_uuid: str
+    old_image_representation_dict: dict,
+    image_2025_04_uuid: str,
+    accession_id: str,
 ) -> bia_data_model.ImageRepresentation:
+    image_representation_dict = copy.deepcopy(old_image_representation_dict)
+
     # Get study accession ID from file uri
-    pattern = r"^.*/(S-[A-Za-z0-9_\-]+)/.*$"
-    matcher = re.compile(pattern)
-    file_uri = ",".join(image_representation_dict["file_uri"])
-    accession_id = matcher.findall(file_uri)[0]
+    # pattern = r"^.*/(S-[A-Za-z0-9_\-]+)/.*$"
+    # matcher = re.compile(pattern)
+    # file_uri = ",".join(image_representation_dict["file_uri"])
+    # accession_id = matcher.findall(file_uri)[0]
     study_uuid = uuid_creation.create_study_uuid(accession_id)
 
-    # TODO: Discuss what unique_str for image_rep uuid should be with Team.
-    # For now using str(use_type)+format as unique string
-    if isinstance(
-        image_representation_dict["use_type"],
-        semantic_models.ImageRepresentationUseType,
-    ):
-        use_type = image_representation_dict["use_type"].value
+    image_representation_dict["model"] = {
+        "type_name": "ImageRepresentation",
+        "version": 4,
+    }
+
+    use_type = image_representation_dict.pop("use_type")
+    if use_type == "UPLOADED_BY_SUBMITTER":
+        unique_string = f"{image_2025_04_uuid}"
+        object_creator = "bia_image_assignment"
+    elif use_type == "INTERACTIVE_DISPLAY":
+        conversion_function = {
+            "conversion_function": "map_image_representation_to_2025_04_model"
+        }
+        unique_string = f"{conversion_function}"
+        object_creator = "bia_image_conversion"
     else:
-        use_type = image_representation_dict["use_type"]
-    unique_string = f"{use_type}{image_representation_dict['image_format']}"
+        raise Exception(f"Use type{use_type} is not mapped to 2025/04 models")
     image_representation_uuid = uuid_creation.create_image_representation_uuid(
         study_uuid, unique_string
     )
 
     image_representation_dict["uuid"] = image_representation_uuid
     image_representation_dict["representation_of_uuid"] = image_2025_04_uuid
-    image_representation_dict["object_creator"] = (
-        semantic_models.Provenance.bia_image_conversion
-    )
+    image_representation_dict["object_creator"] = object_creator
     image_representation_dict["voxel_physical_size_x"] = image_representation_dict.pop(
         "physical_size_x"
     )
@@ -47,10 +81,10 @@ def map_image_representation_to_2025_04_model(
     )
     image_representation_dict["additional_metadata"].append(
         {
-            "provenance": semantic_models.Provenance.bia_ingest,
+            "provenance": object_creator,
             "name": "uuid_unique_input",
             "value": {
-                "uuid_unique_input": f"{use_type}{image_representation_dict['image_format']}"
+                "uuid_unique_input": unique_string,
             },
         }
     )
--- bia-assign-image/tests/test_map_image_related_artefacts_to_2025_04_models.py
+++ bia-assign-image/tests/test_map_image_related_artefacts_to_2025_04_models.py
@@ -16,14 +16,15 @@ os.environ["PYTHONPATH"] = ";".join(
 )
 from scripts.map_image_related_artefacts_to_2025_04_models import (
     map_image_representation_to_2025_04_model,
+    map_image_to_2025_04_model,
 )
 
-accession_id = "S-BIAD-MAP-TO-2025-04-MODELS-TEST"
+accession_id = "S-BIAD-MAP-IMAGE-RELATED-ARTEFACTS-TO-2025-04-MODELS-TEST"
 
 
 @pytest.fixture
 def pre_2025_04_image_representation_dict() -> dict:
-    return {
+    model_dict = {
         "uuid": "570a177d-de5d-4bee-bbd1-fd2dcbec9e2a",
         "representation_of_uuid": "e7322131-8e27-455d-b9fc-8add0719af70",
         "use_type": "INTERACTIVE_DISPLAY",
@@ -31,9 +32,6 @@ def pre_2025_04_image_representation_dict() -> dict:
         "attribute": [],
         "total_size_in_bytes": 0,
         "version": 0,
-        "file_uri": [
-            "https://uks3/testbucket-bia/S-BIAD-MAP-TO-2025-04-MODELS-TEST/image_uuid/image_representation_uuid.ome.zarr",
-        ],
         "physical_size_x": 1,
         "physical_size_y": 1,
         "physical_size_z": 1,
@@ -45,23 +43,25 @@ def pre_2025_04_image_representation_dict() -> dict:
         "image_viewer_setting": [],
         "model": {"type_name": "ImageRepresentation", "version": 3},
     }
+    file_uri = [
+        f"https://uks3/testbucket-bia/{accession_id}/{model_dict['representation_of_uuid']}/{model_dict['uuid']}.ome.zarr",
+    ]
+    model_dict["file_uri"] = file_uri
+    return model_dict
 
 
 @pytest.fixture
 def expected_2025_04_image_representation(
+    pre_2025_04_image_representation_dict,
     expected_2025_04_image,
 ) -> bia_data_model.ImageRepresentation:
     image_representation_dict = {
-        "uuid": "8d225268-d47b-4512-8cf3-0ba14a8be541",
+        "uuid": "fee02b2d-2e5c-4994-9588-7763e8c26bee",
         "representation_of_uuid": str(expected_2025_04_image.uuid),
-        "use_type": "INTERACTIVE_DISPLAY",
         "image_format": ".ome.zarr",
         "total_size_in_bytes": 0,
         "version": 0,
         "object_creator": semantic_models.Provenance.bia_image_conversion,
-        "file_uri": [
-            "https://uks3/testbucket-bia/S-BIAD-MAP-TO-2025-04-MODELS-TEST/image_uuid/image_representation_uuid.ome.zarr",
-        ],
         "voxel_physical_size_x": 1,
         "voxel_physical_size_y": 1,
         "voxel_physical_size_z": 1,
@@ -71,23 +71,25 @@ def expected_2025_04_image_representation(
         "size_c": 1,
         "size_t": 1,
         "image_viewer_setting": [],
-        # TODO: confirm with FS if "version" here should be 4
-        "model": {"type_name": "ImageRepresentation", "version": 3},
+        "model": {"type_name": "ImageRepresentation", "version": 4},
         "additional_metadata": [
             {
-                "provenance": semantic_models.Provenance.bia_ingest,
+                "provenance": semantic_models.Provenance.bia_image_conversion,
                 "name": "uuid_unique_input",
-                # using str(use_type)+image_format as unique string
-                "value": {"uuid_unique_input": "INTERACTIVE_DISPLAY.ome.zarr"},
+                "value": {
+                    "uuid_unique_input": "{'conversion_function': 'map_image_representation_to_2025_04_model'}"
+                },
             }
         ],
     }
+    image_representation_dict["file_uri"] = pre_2025_04_image_representation_dict[
+        "file_uri"
+    ]
     return bia_data_model.ImageRepresentation.model_validate(image_representation_dict)
 
 
 @pytest.fixture
-def expected_2025_04_image() -> bia_data_model.Image:
-    # TODO: Compute proper uuids (i.e. based on actual study_uuid for file reference, creation process and dataset)
+def pre_2025_04_image_dict() -> dict:
     image_dict = {
         "uuid": "b3f9004c-8c9a-4344-bffc-cab4bcf52a10",
         "version": 0,
@@ -116,6 +118,67 @@ def expected_2025_04_image() -> bia_data_model.Image:
         "submission_dataset_uuid": "ae32e92d-84f9-4119-82d1-70cb36477bc0",
         "object_creator": semantic_models.Provenance.bia_image_assignment,
     }
+    return image_dict
+
+
+@pytest.fixture
+def expected_2025_04_image() -> bia_data_model.Image:
+    # image_dict = {
+    #    "uuid": "b3f9004c-8c9a-4344-bffc-cab4bcf52a10",
+    #    "version": 0,
+    #    "model": {"type_name": "Image", "version": 2},
+    #    "additional_metadata": [
+    #        {
+    #            "provenance": semantic_models.Provenance.bia_image_assignment,
+    #            "name": "attributes_from_file_reference_0797dc78-5993-4add-b0fc-235c52055b97",
+    #            "value": {
+    #                "attributes": [
+    #                    "3d_multichannel_time_series/image_01_channel_00_slice_001_time0000.tiff"
+    #                ]
+    #            },
+    #        },
+    #        {
+    #            "provenance": semantic_models.Provenance.bia_ingest,
+    #            "name": "uuid_unique_input",
+    #            # using "".join(original_file_reference_uuid) as unique string
+    #            "value": {"uuid_unique_input": "0797dc78-5993-4add-b0fc-235c52055b97"},
+    #        },
+    #    ],
+    #    "original_file_reference_uuid": [
+    #        "80ef897d-571f-401e-aeab-73c5da842c3b",
+    #    ],
+    #    "creation_process_uuid": "",
+    #    "submission_dataset_uuid": "df381b39-0768-493e-90ed-7c653f012f1f",
+    #    "object_creator": semantic_models.Provenance.bia_image_assignment,
+    # }
+    image_dict = {
+        "object_creator": "bia_image_assignment",
+        "uuid": "93de26f0-6d84-42a5-b6fc-fcd67a94f18e",
+        "version": 0,
+        "model": {"type_name": "Image", "version": 2},
+        "additional_metadata": [
+            {
+                "provenance": "bia_image_assignment",
+                "name": "attributes_from_file_reference_80ef897d-571f-401e-aeab-73c5da842c3b",
+                "value": {"file_list_col1": "col1value", "file_list_col2": "col2value"},
+            },
+            {
+                "provenance": "bia_image_assignment",
+                "name": "file_pattern",
+                "value": {
+                    "file_pattern": "S-BIAD-MAP-IMAGE-RELATED-ARTEFACTS-TO-2025-04-MODELS-TEST/file_reference1.tif"
+                },
+            },
+            {
+                "provenance": "bia_image_assignment",
+                "name": "uuid_unique_input",
+                "value": {"uuid_unique_input": "80ef897d-571f-401e-aeab-73c5da842c3b"},
+            },
+        ],
+        "submission_dataset_uuid": "df381b39-0768-493e-90ed-7c653f012f1f",
+        "creation_process_uuid": "af26940e-35c7-470a-8037-0a27d45253b9",
+        "original_file_reference_uuid": ["80ef897d-571f-401e-aeab-73c5da842c3b"],
+    }
     return bia_data_model.Image.model_validate(image_dict)
 
 
@@ -125,6 +188,31 @@ def test_map_image_representation(
     expected_2025_04_image,
 ):
     mapped_image_representation = map_image_representation_to_2025_04_model(
-        pre_2025_04_image_representation_dict, str(expected_2025_04_image.uuid)
+        pre_2025_04_image_representation_dict,
+        str(expected_2025_04_image.uuid),
+        accession_id,
     )
     assert mapped_image_representation == expected_2025_04_image_representation
+
+
+# Test mapping UPLOADED_BY_SUBMITTER
+
+# Test mapping THUMBNAIL AND STATIC_DISPLAY
+
+
+def test_map_image(
+    pre_2025_04_image_dict,
+    expected_2025_04_image,
+):
+    mapped_image = map_image_to_2025_04_model(
+        pre_2025_04_image_dict,
+        accession_id,
+        file_reference_uuids_2025_04=expected_2025_04_image.original_file_reference_uuid,
+        dataset_uuid_2025_04=expected_2025_04_image.submission_dataset_uuid,
+        api_target="local",
+    )
+    assert mapped_image == expected_2025_04_image
+
+
+def test_update_mapped_image_from_pre_2025_04_representations_linking_image():
+    pass
