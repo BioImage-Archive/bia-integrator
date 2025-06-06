3e31515b513a70de5d409f55f0f1c4d854adfc69
Francois <fsherwood@ebi.ac.uk>
2025-06-06 15:12:25 +0100

    added logic to find input image for creation process

--- bia-assign-image/bia_assign_image/image_assignment.py
+++ bia-assign-image/bia_assign_image/image_assignment.py
@@ -1,13 +1,12 @@
 from bia_assign_image.utils import get_value_from_attribute_list
-from bia_integrator_api import models
 from pydantic import BaseModel, Field
 from typing import Optional
 import logging
 from bia_assign_image.api_client import store_object_in_api_idempotent
-from bia_assign_image.object_creation import (
-    specimen,
-    creation_process,
-)
+from bia_assign_image.object_creation import specimen, creation_process, image
+from bia_integrator_api.api import PrivateApi
+from bia_integrator_api import exceptions, models
+from bia_shared_datamodels import uuid_creation
 
 logger = logging.getLogger("__main__." + __name__)
 
@@ -59,7 +58,7 @@ class ImageDependencies(BaseModel):
 
     def has_dependencies_for_creation_process_creation(self) -> bool:
         if (
-            not self.has_typical_annotation_image_leaf_objects
+            not not self.has_typical_annotation_image_leaf_objects
             and not self.has_typical_rembi_image_leaf_objects
         ):
             return False
@@ -69,6 +68,8 @@ class ImageDependencies(BaseModel):
                 return True
             else:
                 False
+
+        return True
 
     def has_dependencies_for_image_creation(self) -> bool:
         cases_to_test = [
@@ -82,7 +83,9 @@ class ImageDependencies(BaseModel):
         return all(cases_to_test)
 
 
-def find_exisiting_image_dependencies(file_references, api_client):
+def find_exisiting_image_dependencies(
+    file_references: models.FileReference, api_client: PrivateApi
+):
     file_ref_uuids = [f.uuid for f in file_references]
     dataset_uuids = [f.submission_dataset_uuid for f in file_references]
     assert len(set(dataset_uuids)) == 1
@@ -99,17 +102,15 @@ def find_exisiting_image_dependencies(file_references, api_client):
         dataset.additional_metadata, "bio_sample_uuid"
     )
     annotation_method_uuid = get_value_from_attribute_list(
-        dataset.additional_metadata, "annotation_method_uuid", []
+        dataset.additional_metadata, "annotation_method_uuid"
     )
 
     # TODO: check what ingest stores for protocol uuid as we do not want to use growth protocols here.
     protocol_uuid = []
 
-    input_image_uuid = []
-    for file_ref in file_references:
-        # TODO: logic to find something about the source image path & then work out the input image uuid from there?
-        # Or add it to ingest logic
-        pass
+    input_image_uuid = find_input_image_dependencies(
+        file_references, api_client, study_uuid=dataset.submitted_in_study_uuid
+    )
 
     return ImageDependencies(
         dataset_uuid=submission_dataset_uuid,
@@ -121,6 +122,40 @@ def find_exisiting_image_dependencies(file_references, api_client):
         input_image_uuid=input_image_uuid,
         protocol_uuid=protocol_uuid,
     )
+
+
+def find_input_image_dependencies(
+    file_references: models.FileReference, api_client: PrivateApi, study_uuid: str
+):
+    input_image_uuid_list = []
+
+    input_file_ref_uuid_list = []
+    for file_ref in file_references:
+        # TODO: logic to find something about the source image path & then work out the input image uuid from there?
+        # Or add it to ingest logic
+        input_file_ref_uuid_list.extend(
+            get_value_from_attribute_list(
+                file_ref.additional_metadata, "source_image_uuid"
+            )
+        )
+
+    if len(input_file_ref_uuid_list) > 0:
+        input_file_ref_uuid_list = list(set(input_file_ref_uuid_list))
+        input_image_unique_str = image.create_image_uuid_unique_string(
+            input_file_ref_uuid_list
+        )
+        input_image_uuid = uuid_creation.create_image_uuid(
+            study_uuid, input_image_unique_str
+        )
+        try:
+            input_image = api_client.get_image(str(input_image_uuid))
+        except exceptions.NotFoundException:
+            raise RuntimeError(
+                f"Could not find expected dependency Image {input_image_uuid} that is expected to have been created from file references: {input_file_ref_uuid_list}"
+            )
+        input_image_uuid_list.append(input_image.uuid)
+
+    return input_image_uuid_list
 
 
 def create_missing_dependencies(
@@ -154,12 +189,15 @@ def create_missing_dependencies(
 
         existing_dependencies.specimen_uuid = bia_specimen.uuid
 
-    assert existing_dependencies.has_dependencies_for_creation_process_creation()
+    if not existing_dependencies.has_dependencies_for_creation_process_creation():
+        raise ValueError(
+            f"Missing dependencies for CreationProcess creation. Found: {existing_dependencies}"
+        )
 
     bia_creation_process = creation_process.get_creation_process(
         study_uuid,
         image_uuid,
-        bia_specimen.uuid,
+        existing_dependencies.specimen_uuid,
         existing_dependencies.image_acquisition_protocol_uuid,
         existing_dependencies.input_image_uuid,
         existing_dependencies.annotation_method_uuid,
