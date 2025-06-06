4fd6563e31d2d8be8b7bb5601cec5411f85dca79
Francois <fsherwood@ebi.ac.uk>
2025-06-06 17:01:06 +0100

    added tests for annotation image assignment

--- bia-assign-image/tests/test_bia_assign_image_cli.py
+++ bia-assign-image/tests/test_bia_assign_image_cli.py
@@ -1,18 +1,13 @@
 from pathlib import Path
-from typing import Dict
 from typer.testing import CliRunner
 from ruamel.yaml import YAML
-
+from pydantic.alias_generators import to_snake
 import pytest
-from bia_shared_datamodels import bia_data_model
+from bia_integrator_api import models as api_models
 from bia_assign_image import cli
 from .conftest import get_expected_object
+from csv import DictWriter
 
-# from bia_test_data.mock_objects.mock_object_constants import accession_id
-# from bia_test_data.mock_objects import (
-#    mock_image,
-#    mock_image_representation,
-# )
 from bia_assign_image.api_client import get_api_client
 
 
@@ -21,65 +16,76 @@ from bia_assign_image.api_client import get_api_client
 ACCESSION_ID = "S-BIAD-TEST-ASSIGN-IMAGE"
 EXPECTED_OBJECT_BASE_PATH = Path(__file__).parent / "test_data"
 
-
-@pytest.fixture
-def expected_bia_image() -> bia_data_model.Image:
-    expected_image_uuid = "97456be4-fd3b-4303-bff3-02b93d00bd8e"
-    return get_expected_object(
-        EXPECTED_OBJECT_BASE_PATH, "Image", ACCESSION_ID, expected_image_uuid
-    )
+rembi_file_ref_uuid = "278ffc50-3924-4b8a-bad6-d017c503e5dd"
+annotation_file_ref_uuid = "c252f796-fd5c-416b-bfaa-3c2554492b68"
 
 
-@pytest.fixture
-def expected_uploaded_by_submitter_representation() -> (
-    bia_data_model.ImageRepresentation
-):
-    uuid = "a0fbc4fd-2e52-424f-b8e7-6f9fd5109513"
-    return get_expected_object(
-        EXPECTED_OBJECT_BASE_PATH, "ImageRepresentation", ACCESSION_ID, uuid
-    )
-
-
-@pytest.fixture
-def proposal_details_for_input(expected_bia_image) -> Dict:
-    return {
-        "accession_id": ACCESSION_ID,
-        "study_uuid": "dummy_study_uuid",
-        "dataset_uuid": "dummy_dataset_uuid",
-        "name": "dummy_name",
-        "file_reference_uuid": f"{expected_bia_image.original_file_reference_uuid[0]}",
-        "size_in_bytes": "0",
-        "size_human_readable": "dummy_size_human_readable",
-    }
-
-
-@pytest.fixture
-def assign_from_proposal_input_path_tsv(tmpdir, proposal_details_for_input) -> Path:
+def proposal_input_path_tsv(root_path, proposal_contents) -> Path:
     """Return path to a tsv file with proposal details to create mock image"""
 
-    header_row = "\t".join(list(proposal_details_for_input.keys()))
-    values = "\t".join(list(proposal_details_for_input.values()))
+    header_row = list(proposal_contents[0].keys())
 
-    proposal_path = Path(tmpdir) / "proposal_details.tsv"
-    proposal_path.write_text(f"{header_row}\n{values}")
+    proposal_path_tsv = Path(root_path) / "proposal_details.tsv"
+    with open(proposal_path_tsv, "w") as tsv:
+        writer = DictWriter(tsv, header_row, delimiter="\t")
+        writer.writeheader()
+        for row in proposal_contents:
+            writer.writerow(row)
 
-    return proposal_path
+    return proposal_path_tsv.absolute()
 
 
-@pytest.fixture
-def assign_from_proposal_input_path_yaml(tmpdir, proposal_details_for_input) -> Path:
+def proposal_input_path_yaml(root_path, proposal_contents) -> Path:
     """Return path to a yaml file with proposal details to create mock image"""
 
     yaml = YAML(typ="safe")
-    proposal_path = Path(tmpdir) / "proposal_details.yaml"
+    proposal_path = Path(root_path) / "proposal_details.yaml"
     yaml.dump(
-        [
-            proposal_details_for_input,
-        ],
+        proposal_contents,
         proposal_path,
     )
 
-    return proposal_path
+    return proposal_path.absolute()
+
+
+@pytest.fixture
+def expected_objects(request: pytest.FixtureRequest):
+    # Map of starting FileReference uuid to all the uuid of all the objects (and their types) expected to be created by Image Assignment.
+    expected_objects = {
+        "278ffc50-3924-4b8a-bad6-d017c503e5dd": [
+            ("97456be4-fd3b-4303-bff3-02b93d00bd8e", "Image"),
+            ("a0fbc4fd-2e52-424f-b8e7-6f9fd5109513", "ImageRepresentation"),
+            ("94394b72-823e-4565-8131-e4c5e60d45a7", "Specimen"),
+            ("f154164e-7e6e-42f9-943e-c6d1ff0a5ba2", "CreationProcess"),
+        ],
+        "c252f796-fd5c-416b-bfaa-3c2554492b68": [
+            ("b912bebc-8386-47cc-a0fe-0af500d63a8a", "Image"),
+            ("4fd4c5c3-7d43-44fb-b38a-3d16f6b0c2eb", "ImageRepresentation"),
+            ("18fc18d2-f3e0-4b7c-a89d-c62e6b354e26", "CreationProcess"),
+        ],
+    }
+    return expected_objects[request.param]
+
+
+@pytest.fixture()
+def proposal_path_yaml_and_tsv(tmpdir, request: pytest.FixtureRequest):
+
+    proposal_contents = [
+        {
+            "accession_id": ACCESSION_ID,
+            "study_uuid": "dummy_study_uuid",
+            "dataset_uuid": "dummy_dataset_uuid",
+            "name": "dummy_name",
+            "file_reference_uuid": request.param,
+            "size_in_bytes": "0",
+            "size_human_readable": "dummy_size_human_readable",
+        },
+    ]
+
+    return {
+        "tsv": proposal_input_path_tsv(tmpdir, proposal_contents),
+        "yaml": proposal_input_path_yaml(tmpdir, proposal_contents),
+    }
 
 
 @pytest.fixture
@@ -88,33 +94,36 @@ def test_api_client():
 
 
 @pytest.mark.parametrize(
-    "format",
+    "proposal_path_yaml_and_tsv, expected_objects",
     (
-        "tsv",
-        "yaml",
+        [rembi_file_ref_uuid, rembi_file_ref_uuid],
+        [annotation_file_ref_uuid, annotation_file_ref_uuid],
     ),
+    indirect=True,
 )
 def test_cli_assign_from_proposal_command(
     test_api_client,
-    format,
-    assign_from_proposal_input_path_tsv,
-    assign_from_proposal_input_path_yaml,
-    expected_bia_image,
-    expected_uploaded_by_submitter_representation,
+    proposal_path_yaml_and_tsv,
+    expected_objects,
 ):
-    # This implicitly tests the 'assign' and 'create' commands
-    runner = CliRunner()
 
-    if format == "tsv":
-        assign_from_proposal_input_path = f"{assign_from_proposal_input_path_tsv}"
-    elif format == "yaml":
-        assign_from_proposal_input_path = f"{assign_from_proposal_input_path_yaml}"
+    run_cli_and_check_output(
+        test_api_client, proposal_path_yaml_and_tsv["tsv"], expected_objects
+    )
+    run_cli_and_check_output(
+        test_api_client, proposal_path_yaml_and_tsv["yaml"], expected_objects
+    )
+
+
+def run_cli_and_check_output(test_api_client, proposal_input_path, expected_object):
+
+    runner = CliRunner()
 
     result = runner.invoke(
         cli.app,
         [
             "assign-from-proposal",
-            assign_from_proposal_input_path,
+            str(proposal_input_path),
             "--api",
             "local",
         ],
@@ -122,32 +131,18 @@ def test_cli_assign_from_proposal_command(
 
     assert result.exit_code == 0
 
-    # Check BIA image and UPLOADED_BY_SUBMITTER representation objects created.
-    #
-    # TODO: Investigate issue commented on below.
-    # At time of writing test (21/02/2025), there are subtle differences in
-    # API models and bia_data_models (e.g. Pydantic serializer warnings around type of
-    # AttributeProvenance which may be leading to:
-    #     <AttributeProvenance.bia_conversion: 'bia_conversion' (bia_data_model) vs
-    #     <AttributeProvenance.BIA_CONVERSION: 'bia_conversion' (api)
-    # so convert returned value to bia_data_model.* before comparison)
-    created_bia_image_dict = test_api_client.get_image(
-        str(expected_bia_image.uuid)
-    ).model_dump()
-    created_bia_image = bia_data_model.Image.model_validate(created_bia_image_dict)
-    assert created_bia_image == expected_bia_image
-
-    created_uploaded_by_submitter_representation_dict = (
-        test_api_client.get_image_representation(
-            str(expected_uploaded_by_submitter_representation.uuid)
-        ).model_dump()
-    )
-    created_uploaded_by_submitter_representation = (
-        bia_data_model.ImageRepresentation.model_validate(
-            created_uploaded_by_submitter_representation_dict
+    for object_uuid, type_name in expected_object:
+        expected_object = get_expected_object(
+            EXPECTED_OBJECT_BASE_PATH, type_name, ACCESSION_ID, object_uuid
         )
-    )
-    assert (
-        created_uploaded_by_submitter_representation
-        == expected_uploaded_by_submitter_representation
-    )
+        api_class = getattr(api_models, type_name)
+        expected_object = api_class.model_validate_json(
+            expected_object.model_dump_json()
+        )
+
+        model_name_snake = to_snake(type_name)
+        get_func_name = "get_" + model_name_snake
+        get_function = getattr(test_api_client, get_func_name)
+        object_from_api = get_function(object_uuid)
+
+        assert object_from_api == expected_object
