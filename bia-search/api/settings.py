7ac0d9d2596dfb542365b2b88e2d29bf2b8b7417
Liviu Anita <liviu@ebi.ac.uk>
2025-06-11 14:43:57 +0100

    Fix search ci test

--- bia-search/api/settings.py
+++ bia-search/api/settings.py
@@ -5,8 +5,8 @@ from pathlib import Path
 class Settings(BaseSettings):
     fastapi_root_path: str = ""
 
-    elastic_connstring: str
-    elastic_index: str = "fts_index"
+    elastic_connstring: str = "http://elastic:test@localhost:9200"
+    elastic_index: str = "test_index"
 
     model_config = SettingsConfigDict(
         env_file=Path(__file__).parent / ".env", extra="ignore"
