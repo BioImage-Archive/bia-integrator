e6910acb219864eeac61bb35d4aa68ec66e55052
Kola <kola@ebi.ac.uk>
2025-06-24 17:13:37 +0100

    Save WIP with all basic functionality working and tests passing

---
+++ bia-converter/tests/test_cli_for_creation_of_2d_views.py
@@ -0,0 +1,155 @@
+from pathlib import Path
+import shutil
+import pytest
+import json
+import bia_integrator_api.models as api_models
+from bia_integrator_api.exceptions import NotFoundException
+from bia_converter import io
+from bia_converter import convert
+from typer.testing import CliRunner
+from bia_converter import cli
+from bia_converter.config import settings
+from bia_converter.bia_api_client import api_client
+from bia_converter.utils import attributes_by_name
+
+accession_id = "S-BIAD-BIACONVERTER-TEST"
+original_copy_local_to_s3 = convert.copy_local_to_s3
+
+
+@pytest.fixture
+def mock_copy_local_to_s3(monkeypatch):
+    """Copy files to a temporary directory instead the s3 bucket specified in settings.bucket
+    
+    """
+    def _mock_copy_local_to_s3(src_fpath: Path, dst_key: str) -> str:
+
+        # We call the original with dry_run=True to get the file_uri, but it will not copy
+        file_uri = original_copy_local_to_s3(src_fpath, dst_key, dry_run=True)
+        
+        dst_dir = settings.cache_root_dirpath / "mock_s3"
+        dst_dir.mkdir(exist_ok=True)
+        dst_fname = Path(dst_key).name
+        dst_fpath = dst_dir / dst_fname
+        shutil.copy(src_fpath, dst_fpath)
+        return file_uri
+
+    monkeypatch.setattr(convert, "copy_local_to_s3", _mock_copy_local_to_s3)
+
+
+@pytest.fixture
+def runner() -> CliRunner:
+    return CliRunner()
+
+
+@pytest.fixture
+def expected_thumbnail_uri(input_image_uuid) -> str:
+    return [f"{settings.endpoint_url}/{settings.bucket_name}/{accession_id}/{input_image_uuid}/thumbnail_256x256.png",]
+
+
+@pytest.fixture
+def expected_static_display_uri(input_image_uuid) -> str:
+    return [f"{settings.endpoint_url}/{settings.bucket_name}/{accession_id}/{input_image_uuid}/static_display_512x512.png",]
+
+
+def setup_input_image_and_return_uuid() -> str:
+    input_image_path = Path(__file__).parent / "data" / "input_image_for_creating_2d_views.json"
+    input_image_dict = json.loads(input_image_path.read_text())
+    input_image = api_models.Image.model_validate(input_image_dict)
+    input_image_uuid = f"{input_image.uuid}"
+
+    try:
+        api_version_of_input_image = api_client.get_image(input_image_uuid)
+        input_image.version = api_version_of_input_image.version + 1
+    except NotFoundException:
+        pass
+    api_client.post_image(input_image)
+
+    return input_image_uuid
+
+
+def setup_interactive_image_rep_and_return_uuid() -> str:
+
+    interactive_image_rep_uuid = "841aaca8-633d-48f9-bfdb-6fe765aad109"
+    try:
+        interactive_image_rep = api_client.get_image_representation(interactive_image_rep_uuid)
+    except NotFoundException:
+        interactive_image_rep_path = Path(__file__).parent / "data" / "interactive_image_representation_for_creating_2d_views.json"
+        interactive_image_rep_dict = json.loads(interactive_image_rep_path.read_text())
+        interactive_image_rep = api_models.ImageRepresentation.model_validate(interactive_image_rep_dict)
+        assert interactive_image_rep_uuid == interactive_image_rep.uuid
+
+        # Ensure the file_uri points to the ome.zarr in tests/data
+        file_uri_path = Path(__file__).parent.absolute() / "data" / "im07.ome.zarr" / "0"
+        interactive_image_rep.file_uri = [f"file://{file_uri_path}"]
+
+        api_client.post_image_representation(interactive_image_rep)
+
+    return interactive_image_rep_uuid
+
+@pytest.fixture
+def input_image_uuid() -> str:
+    return setup_input_image_and_return_uuid()
+
+@pytest.fixture
+def interactive_image_rep_uuid(input_image_uuid) -> str:
+
+    # The input_image_uuid parameter in func declaration is used to ensure the input image
+    # is setup before the interactive image representation which depends on it.
+    return setup_interactive_image_rep_and_return_uuid()
+
+def test_cli_convert_interactive_display_to_static_display(
+    runner,
+    interactive_image_rep_uuid,
+    mock_copy_local_to_s3,
+    expected_static_display_uri,
+    input_image_uuid,
+):
+    result = runner.invoke(
+        cli.app,
+        [
+            "create-static-display",
+            interactive_image_rep_uuid,
+        ],
+        catch_exceptions=False,
+    )
+
+    assert result.exit_code == 0
+
+    updated_image = api_client.get_image(input_image_uuid)
+    attributes = attributes_by_name(updated_image)
+    key = "image_static_display_uri"
+    assert key in attributes
+    assert attributes[key][key] == expected_static_display_uri
+    created_static_display_path = (
+        settings.cache_root_dirpath / "mock_s3" / "static_display_512x512.png"
+    )
+    assert created_static_display_path.exists()
+
+
+
+def test_cli_convert_interactive_display_to_thumbnail(
+    runner,
+    interactive_image_rep_uuid,
+    mock_copy_local_to_s3,
+    expected_thumbnail_uri,
+    input_image_uuid,
+):
+    result = runner.invoke(
+        cli.app,
+        [
+            "create-thumbnail",
+            interactive_image_rep_uuid,
+        ],
+        catch_exceptions=False,
+    )
+
+    assert result.exit_code == 0
+
+    updated_image = api_client.get_image(input_image_uuid)
+    attributes = attributes_by_name(updated_image)
+    assert "image_thumbnail_uri" in attributes
+    assert attributes["image_thumbnail_uri"]["image_thumbnail_uri"] == expected_thumbnail_uri
+    created_thumbnail_path = (
+        settings.cache_root_dirpath / "mock_s3" / "thumbnail_256x256.png"
+    )
+    assert created_thumbnail_path.exists()
