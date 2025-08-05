dea4086543ff94a7712e8dda025ca1f82766f2ec
Tushar <tushar@ebi.ac.uk>
2025-08-05 15:13:23 +0100

    Nueroglancer url script - Script update: Read Image JSON and output overlays JSON - Script update: Update crossSectionScale based on image size - Script update: Update layout based on Z-dimension - ReadMe Update - Poetry add NGFF_ZARR package

--- bia-converter/scripts/generate_neuroglancer_link.py
+++ bia-converter/scripts/generate_neuroglancer_link.py
@@ -1,17 +1,98 @@
 import json
 import ngff_zarr as nz
+from pathlib import Path
 from numpy import percentile
 import urllib.parse
 import typer
-from typing_extensions import Annotated
 
 app = typer.Typer()
 
-def getContrastValues(source_url, scale_level=0, use_percentiles=True):
+def generateImageMap(images):
+    FILTER = [
+        "Raw image in JPEG format",
+        "Visualization of groundtruth masks in PNG format",
+        "Visualization of groundtruth for randomly selected nuclei in PNG format"
+    ]
+    def is_valid_rep(rep):
+            return ( rep.get("image_format") == ".ome.zarr" and
+                not any(
+                    md.get("value", {}).get("attributes", {}).get("file description") in FILTER
+                    for md in img.get("additional_metadata", [])
+                )
+            )
+    
+    def checkVoxels(voxels):
+        return [1e-6 if voxel is None else voxel for voxel in voxels]
+
+    def getVoxelsFromImageRep(image_rep):
+        return checkVoxels((image_rep["voxel_physical_size_x"], image_rep["voxel_physical_size_y"], image_rep["voxel_physical_size_z"], image_rep["size_c"], image_rep["size_t"]))
+    
+    def fileFilter(img):
+        metadata = img.get("additional_metadata", [])
+        for md in metadata:
+            fd = md.get("value", {}).get("attributes", {})
+            if fd and fd.get("file description") in FILTER:
+                return False
+        return True
+    
+    annotated_images_map = {}
+    
+    for img in images.values():
+        input_image_uuid = img.get("creation_process", {}).get("input_image_uuid", [])
+        if not input_image_uuid or not fileFilter(img):
+            continue
+        key = input_image_uuid[0]
+        if key not in images:
+            continue
+
+        valid_rep_source = [rep for rep in images[key].get("representation", []) if is_valid_rep(rep)]
+        if not valid_rep_source:
+            continue
+
+        source_rep = valid_rep_source[0]
+        source_url = source_rep["file_uri"][0]
+        source_voxel = getVoxelsFromImageRep(source_rep)
+
+        if key not in annotated_images_map:
+            annotated_images_map[key] = {
+                "source_url": source_url,
+                "source_voxel": source_voxel,
+                "annotations": []
+            }
+
+        annotations = [
+            {
+                "url": rep["file_uri"][0],
+                "voxels": getVoxelsFromImageRep(rep)
+            }
+            for rep in img.get("representation", []) if is_valid_rep(rep)
+        ]
+
+        if not annotations:
+            continue
+
+        annotated_images_map[key]["annotations"].append({
+            "annotation_uuid": img["uuid"],
+            "annotation_url": [ann["url"] for ann in annotations],
+            "annotation_voxels": [ann["voxels"] for ann in annotations]
+        })
+
+    return annotated_images_map
+
+def writeJSON(filepath, data):
+    with open(filepath, "w") as f:
+        json.dump(data, f, indent=2)
+
+def readJSON(path):
+    with open(path) as f:
+        return json.load(f)
+
+def getContrastValuesAndXYZ(source_url, scale_level=0, use_percentiles=True):
     source = nz.from_ngff_zarr(source_url)
     image = source.images[scale_level]
     data = image.data
-    
+    x, y, z = data.shape[4], data.shape[3], data.shape[2]
+
     if use_percentiles:
         min_val = percentile(data.__array__(), 1)
         max_val = percentile(data.__array__(), 99)
@@ -19,7 +100,7 @@ def getContrastValues(source_url, scale_level=0, use_percentiles=True):
         min_val = min(data.__array__())
         max_val = max(data.__array__())
         
-    return[min_val, max_val]
+    return [[min_val, max_val], [x,y,z]]
 
 def getDimensions(dims):
     dims = processXYVoxels(dims)
@@ -41,7 +122,7 @@ def getTranformations(output_dims, input_dims):
 def getTransformedLayer(source, source_voxel, annotated_voxel):
     return {"url": f"{source}/|zarr2:", "transform": getTranformations(source_voxel, annotated_voxel)}
 
-def getLayer(source, layer_type, name, source_voxel, annotated_voxels=None):
+def getLayer(source, layer_type, name, source_voxel, annotated_voxels, contrast=None):
     if annotated_voxels:
         if source_voxel[0] > annotated_voxels[0]:
             source_transformation =  getTransformedLayer(source, annotated_voxels, source_voxel)
@@ -49,7 +130,8 @@ def getLayer(source, layer_type, name, source_voxel, annotated_voxels=None):
             source_transformation = getTransformedLayer(source, annotated_voxels, source_voxel)
         else:
             source_transformation =  getTransformedLayer(source, source_voxel, source_voxel)
-    shaders = {"normalized": {"range": getContrastValues(source)}} if layer_type == "image" else {}
+
+    shaders = {"normalized": {"range": contrast}} if layer_type == "image" else {}
     return {
         "type": layer_type,
         "source": source_transformation,
@@ -69,20 +151,27 @@ def processXYVoxels(obj):
 
 def generateNeuroglancerUrl(source_url, segmentation_urls, source_voxel=(1,1,1,1,1), annotation_voxels=[(1,1,1,1,1)], layout="xy"):
     source_label = "Source Image"
-    layers = [getLayer(source_url, "image", source_label, source_voxel, annotation_voxels[0])]
+    contrast, xyz = getContrastValuesAndXYZ(source_url)
+    x,y,z = xyz
+    layers = [getLayer(source_url, "image", source_label, source_voxel, annotation_voxels[0], contrast)]
     dimensions = getDimensions(source_voxel)
 
     for i, seg_url in enumerate(segmentation_urls):
         layers.append(getLayer(seg_url, "segmentation", f"Annotation {i+1}", source_voxel, annotation_voxels[i]))
 
-    display_dimension = ["x", "y", "z"] if int(source_voxel[2]) != 1 else ["x", "y"]
-    layout = "4panel-alt" if int(source_voxel[2]) != 1 else "xy" 
+    imageHasZDims = int(source_voxel[2]) != 1 and int(annotation_voxels[0][2]) != 1 and int(z) != 1
+    
+    display_dimension = ["x", "y", "z"] if imageHasZDims else ["x", "y"]
+    layout = "4panel-alt" if imageHasZDims else "xy" 
+    position = [0, 0, 0, int(y/2), int(x/2)] if imageHasZDims else [0, 0, int(y/2), int(x/2)]
+    cross_section_scale = 3 if int(x) > 2000 else 1
+
 
     state = {
         "dimensions": dimensions,
         "displayDimensions": display_dimension,
-        "position": [0, 0, 1, 1],
-        "crossSectionScale": 1,
+        "position": position,
+        "crossSectionScale": cross_section_scale,
         "projectionScale": 512,
         "layers": layers,
         "layout": layout
@@ -93,14 +182,34 @@ def generateNeuroglancerUrl(source_url, segmentation_urls, source_voxel=(1,1,1,1
     return f"https://neuroglancer-demo.appspot.com/#!{encoded_json}"
 
 @app.command()
-def main(source: Annotated[str, typer.Argument(..., help="OME ZARR URL for the source image")],
-         segmentation: Annotated[str, typer.Argument(..., help="a List of OME ZARR URLs for the annotated image")],
-         source_voxel: Annotated[str, typer.Argument(..., help="source image voxels")],
-         annotation_voxels: Annotated[str, typer.Argument(..., help="a List of annotated image voxels")],
-         ):
-    
-    url = generateNeuroglancerUrl(source, segmentation, source_voxel, annotation_voxels)
-    return url
+def main(images_path: Path = typer.Option(..., help="Path to raw OME-Zarr JSON")):
+    annotated_image_map = generateImageMap(readJSON(images_path))
+
+    uuid_to_skip = []
+    overlays = {}
+
+    for key, values in annotated_image_map.items():
+        if key in uuid_to_skip or not values:
+            continue
+
+        source_image_url = values["source_url"]
+        source_voxels = values["source_voxel"]
+
+        if not source_image_url:
+            continue
+
+        annotated_images = values["annotations"]
+        annotation_urls = [a["annotation_url"][0] for a in annotated_images if a["annotation_url"]]
+        annotation_voxels = [a["annotation_voxels"][0] for a in annotated_images if a["annotation_voxels"]]
+
+        if not annotation_voxels:
+            print(f"Skipping {key} due to missing annotation voxels.")
+            continue
+        url = generateNeuroglancerUrl(source_image_url, annotation_urls, source_voxels, annotation_voxels)
+        overlays[key] = url
+
+    writeJSON("overlay_neuroglancer_links.json", overlays)
+
 
 if __name__ == "__main__":
     app()
