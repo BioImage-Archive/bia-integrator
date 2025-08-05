e05d3672337a8d5221707500a88dbef881b2faec
Tushar <tushar@ebi.ac.uk>
2025-08-05 13:19:59 +0100

    tidying up

--- bia-converter/scripts/generate_neuroglancer_link.py
+++ bia-converter/scripts/generate_neuroglancer_link.py
@@ -1,48 +1,63 @@
 import json
+import ngff_zarr as nz
+from numpy import percentile
 import urllib.parse
 import typer
 from typing_extensions import Annotated
 
 app = typer.Typer()
 
-def get_tranformations(src, ann):
+def getContrastValues(source_url, scale_level=0, use_percentiles=True):
+    source = nz.from_ngff_zarr(source_url)
+    image = source.images[scale_level]
+    data = image.data
+    
+    if use_percentiles:
+        min_val = percentile(data.__array__(), 1)
+        max_val = percentile(data.__array__(), 99)
+    else:
+        min_val = min(data.__array__())
+        max_val = max(data.__array__())
+        
+    return[min_val, max_val]
+
+def getDimensions(dims):
+    dims = processXYVoxels(dims)
+    units = "m" if dims[0] < 1 else "um"
     return {
-        "outputDimensions": {
-            "t": [src[4], ""],
-            "c": [src[3], ""],
-            "z": [float(src[2]), ""],
-            "y": [float(src[1]), "um"],
-            "x": [float(src[0]), "um"]
-        },
-        "inputDimensions": {
-            "t": [ann[4], ""],
-            "c": [ann[3], ""],
-            "z": [float(ann[2]), ""],
-            "y": [float(ann[1]), "um"],
-            "x": [float(ann[0]), "um"]
+            "t": [dims[4], ""],
+            "c": [dims[3], ""],
+            "z": [float(dims[2]), ""],
+            "y": [float(dims[1]), units],
+            "x": [float(dims[0]), units]
         }
+
+def getTranformations(output_dims, input_dims):
+    return {
+        "outputDimensions": getDimensions(output_dims),
+        "inputDimensions": getDimensions(input_dims)
     }
 
-def get_layer(source, layer_type, name, voxel_size, annotated_voxels=None):
+def getTransformedLayer(source, source_voxel, annotated_voxel):
+    return {"url": f"{source}/|zarr2:", "transform": getTranformations(source_voxel, annotated_voxel)}
+
+def getLayer(source, layer_type, name, source_voxel, annotated_voxels=None):
     if annotated_voxels:
-        return {
-            "type": layer_type,
-            "source": {
-                "url": f"{source}/|zarr2:",
-                "transform": get_tranformations(voxel_size, annotated_voxels)
-            },
-            "name": name,
-            "tab": "source" if layer_type == "image" else "segments",
-            "segments": [] if layer_type == "segmentation" else None
-        }
-    else:
-        return {
-            "type": layer_type,
-            "source": f"{source}/|zarr2:",
-            "name": name,
-            "tab": "source" if layer_type == "image" else "segments",
-            "segments": [] if layer_type == "segmentation" else None
-        }
+        if source_voxel[0] > annotated_voxels[0]:
+            source_transformation =  getTransformedLayer(source, annotated_voxels, source_voxel)
+        elif source_voxel[0] == annotated_voxels[0]:
+            source_transformation = getTransformedLayer(source, annotated_voxels, source_voxel)
+        else:
+            source_transformation =  getTransformedLayer(source, source_voxel, source_voxel)
+    shaders = {"normalized": {"range": getContrastValues(source)}} if layer_type == "image" else {}
+    return {
+        "type": layer_type,
+        "source": source_transformation,
+        "name": name,
+        "tab": "source" if layer_type == "image" else "segments",
+        "shaderControls": shaders,
+        "segments": [] if layer_type == "segmentation" else None
+    }
 
 def convertVoxelSizeToUM(vx):
     return vx * 1e-6 if int(vx) == 1 else vx
@@ -52,36 +67,21 @@ def processXYVoxels(obj):
         obj[i] = convertVoxelSizeToUM(obj[i])
     return obj
 
-def generate_neuroglancer_url(source_url, segmentation_urls, voxel_size=(1,1,1,1,1), annotated_voxel_size=[(1,1,1,1,1)], layout="xy"):
+def generateNeuroglancerUrl(source_url, segmentation_urls, source_voxel=(1,1,1,1,1), annotation_voxels=[(1,1,1,1,1)], layout="xy"):
     source_label = "Source Image"
-    src_vx = voxel_size
-    ann_vx = annotated_voxel_size[0]
-
-    if voxel_size[0] > ann_vx[0]:
-        layers = [get_layer(source_url, "image", source_label, ann_vx, voxel_size)]
-    elif voxel_size[0] == ann_vx[0]:
-        layers = [get_layer(source_url, "image", source_label, voxel_size)]
-    else:
-        layers = [get_layer(source_url, "image", source_label, voxel_size, ann_vx)]
-
-    dimensions = {
-        "x": [float(src_vx[0]), "um"],
-        "y": [float(src_vx[1]), "um"],
-        "z": [float(src_vx[2]), ""],
-        "c": [float(src_vx[3]), ""],
-        "t": [float(src_vx[4]), ""]
-    }
+    layers = [getLayer(source_url, "image", source_label, source_voxel, annotation_voxels[0])]
+    dimensions = getDimensions(source_voxel)
 
     for i, seg_url in enumerate(segmentation_urls):
-        if voxel_size[0] < annotated_voxel_size[i][0]:
-            layers.append(get_layer(seg_url, "segmentation", f"Annotation {i+1}", voxel_size, annotated_voxel_size[i]))
-        else:
-            layers.append(get_layer(seg_url, "segmentation", f"Annotation {i+1}", annotated_voxel_size[i]))
+        layers.append(getLayer(seg_url, "segmentation", f"Annotation {i+1}", source_voxel, annotation_voxels[i]))
+
+    display_dimension = ["x", "y", "z"] if int(source_voxel[2]) != 1 else ["x", "y"]
+    layout = "4panel-alt" if int(source_voxel[2]) != 1 else "xy" 
 
     state = {
         "dimensions": dimensions,
-        "displayDimensions": ["x", "y"],
-        "position": [1, 1, 1, 1],
+        "displayDimensions": display_dimension,
+        "position": [0, 0, 1, 1],
         "crossSectionScale": 1,
         "projectionScale": 512,
         "layers": layers,
@@ -95,11 +95,11 @@ def generate_neuroglancer_url(source_url, segmentation_urls, voxel_size=(1,1,1,1
 @app.command()
 def main(source: Annotated[str, typer.Argument(..., help="OME ZARR URL for the source image")],
          segmentation: Annotated[str, typer.Argument(..., help="a List of OME ZARR URLs for the annotated image")],
-         voxel_size: Annotated[str, typer.Argument(..., help="source image voxels")],
+         source_voxel: Annotated[str, typer.Argument(..., help="source image voxels")],
          annotation_voxels: Annotated[str, typer.Argument(..., help="a List of annotated image voxels")],
          ):
     
-    url = generate_neuroglancer_url(source, segmentation, voxel_size, annotation_voxels)
+    url = generateNeuroglancerUrl(source, segmentation, source_voxel, annotation_voxels)
     return url
 
 if __name__ == "__main__":
