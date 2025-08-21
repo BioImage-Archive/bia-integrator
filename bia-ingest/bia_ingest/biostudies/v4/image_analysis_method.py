2676acb43596b001211acba444e4e7cb09e8bb62
Kola <kola@ebi.ac.uk>
2025-08-21 10:32:20 +0100

    Update comments

--- bia-ingest/bia_ingest/biostudies/v4/image_analysis_method.py
+++ bia-ingest/bia_ingest/biostudies/v4/image_analysis_method.py
@@ -33,6 +33,8 @@ def get_image_analysis_method_as_map(
 
         # KB 20250820. New ST allows empty Image Correlation section. Fix
         # has been requested, but some studies have slipped through.
+        # Without a title, it can't be associated to anything, so we
+        # skip creating them because they won't appear in any dataset.
         if "Title" not in attr_dict:
             continue
 
