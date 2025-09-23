e021e6a18a460830b2ac226f66440b5c3058033e
Francois <fsherwood@ebi.ac.uk>
2025-09-23 09:26:16 +0100

    remove global variables from get_contributors function in empiar

--- ro-crate-ingest/ro_crate_ingest/empiar_to_ro_crate/entity_conversion/contributor.py
+++ ro-crate-ingest/ro_crate_ingest/empiar_to_ro_crate/entity_conversion/contributor.py
@@ -4,20 +4,26 @@ from ro_crate_ingest.empiar_to_ro_crate.empiar.entry_api_models import (
     AuthorEditor,
 )
 
-CONTRIBUTOR_BNODE_INT = 0
-
 
 def get_contributors(empiar_api_entry: Entry) -> list[Contributor]:
+    contributor_bnode_int = 0
     contributors = []
     for author in empiar_api_entry.authors:
-        contributors.append(get_contributor(author.author))
+        contributor, contributor_bnode_int = get_contributor(
+            author.author, contributor_bnode_int
+        )
+        contributors.append(contributor)
     return contributors
 
 
-def get_contributor(author: AuthorEditor) -> Contributor:
+def get_contributor(author: AuthorEditor, contributor_bnode_int: int) -> Contributor:
+    contributor_id, contributor_bnode_int = get_contributor_id(
+        author, contributor_bnode_int
+    )
+
     model_dict = {
         "@type": ["Person", "bia:Contributor"],
-        "@id": get_contributor_id(author),
+        "@id": contributor_id,
         "displayName": author.name,
         "address": None,
         "website": None,
@@ -26,18 +32,19 @@ def get_contributor(author: AuthorEditor) -> Contributor:
         "contactEmail": None,
     }
 
-    return Contributor(**model_dict)
+    return Contributor(**model_dict), contributor_bnode_int
 
 
-def get_contributor_id(author: AuthorEditor):
+def get_contributor_id(
+    author: AuthorEditor, contributor_bnode_int: int
+) -> tuple[str, int]:
 
     if author.author_orcid:
         id: str = author.author_orcid
         if not id.startswith("https://orcid.org/"):
             id = f"https://orcid.org/{id}"
     else:
-        global CONTRIBUTOR_BNODE_INT
-        id = f"_:c{CONTRIBUTOR_BNODE_INT}"
-        CONTRIBUTOR_BNODE_INT += 1
+        id = f"_:c{contributor_bnode_int}"
+        contributor_bnode_int += 1
 
-    return id
+    return id, contributor_bnode_int
