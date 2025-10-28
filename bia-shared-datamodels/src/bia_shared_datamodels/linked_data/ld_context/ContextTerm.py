69fde43b03663be22ef7091311f39f0303daf276
Francois <fsherwood@ebi.ac.uk>
2025-10-28 13:24:22 +0000

    added handling for @reverse context keyword

--- bia-shared-datamodels/src/bia_shared_datamodels/linked_data/ld_context/ContextTerm.py
+++ bia-shared-datamodels/src/bia_shared_datamodels/linked_data/ld_context/ContextTerm.py
@@ -1,32 +1,41 @@
-from typing import Optional
 from rdflib import URIRef
 
 
 class ContextTerm:
     full_uri: URIRef
     field_name: str
-    type_mapping: Optional[str]
+    type_mapping: str | None
+    is_reverse: bool
 
     def __init__(
         self,
         full_uri: str | URIRef,
         field_name: str,
-        type_mapping: Optional[str] = None,
+        type_mapping: str | None = None,
+        is_reverse: bool = False,
     ):
         self.full_uri = URIRef(full_uri)
         self.field_name = field_name
         self.type_mapping = type_mapping
+        self.is_reverse = is_reverse
 
     def __eq__(self, other):
         if not isinstance(other, ContextTerm):
             return False
 
-        return self.full_uri == other.full_uri and self.field_name == other.field_name
+        return all(
+            [
+                self.full_uri == other.full_uri,
+                self.field_name == other.field_name,
+            ]
+        )
 
     def __hash__(self):
-        return hash((self.full_uri, self.field_name, self.type_mapping))
+        return hash(
+            (self.full_uri, self.field_name, self.type_mapping, self.is_reverse)
+        )
 
-    def to_context_term_dict(self, prefixes: Optional[dict[str, str]] = None) -> dict:
+    def to_context_term_dict(self, prefixes: dict[str, str] | None = None) -> dict:
         context_term = {}
 
         id = self.full_uri
@@ -42,14 +51,17 @@ class ContextTerm:
                     if len(compact_id) < len(id):
                         id = compact_id
 
-        context_term |= {"@id": id}
+        if self.is_reverse:
+            context_term |= {"@reverse": id}
+        else:
+            context_term |= {"@id": id}
 
         if self.type_mapping:
             context_term |= {"@type": self.type_mapping}
         return context_term
 
     def to_mapping_dict(
-        self, prefixes: Optional[dict[str, str]] = None
+        self, prefixes: dict[str, str] | None = None
     ) -> dict[str, dict]:
         context_term = self.to_context_term_dict(prefixes)
 
--- bia-shared-datamodels/src/bia_shared_datamodels/linked_data/pydantic_ld/FieldContext.py
+++ bia-shared-datamodels/src/bia_shared_datamodels/linked_data/pydantic_ld/FieldContext.py
@@ -6,7 +6,9 @@ class FieldContext:
 
     uri: URIRef
     isIdField: bool
+    isReverseField: bool
 
-    def __init__(self, uri: str, isIdField: bool = False):
+    def __init__(self, uri: str, isIdField: bool = False, isReverseField=False):
         self.uri = URIRef(uri)
         self.isIdField = isIdField
+        self.isReverseField = isReverseField
