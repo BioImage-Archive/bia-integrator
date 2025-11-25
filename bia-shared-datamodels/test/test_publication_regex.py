605619b0511ec1a48913b8906f513c0543d2b104
Kola <kola@ebi.ac.uk>
2025-11-25 17:30:27 +0000

    Revise sanitise_doi function as suggested during review

--- bia-shared-datamodels/test/test_publication_regex.py
+++ bia-shared-datamodels/test/test_publication_regex.py
@@ -13,6 +13,7 @@ import pytest
         ("dx.doi.org:10.12345/test", "https://doi.org/10.12345/test"),
         ("doi.org/10.12345/test", "https://doi.org/10.12345/test"),
         ("dx.doi.org/10.12345/test", "https://doi.org/10.12345/test"),
+        ("10.12345/test", "https://doi.org/10.12345/test"),
     ),
 )
 def test_dois_that_should_validate(doi, expected_sanitised_doi):
@@ -28,6 +29,7 @@ def test_dois_that_should_validate(doi, expected_sanitised_doi):
         "https://doi.org/10.1234/",
         "doi.org:10.12345/",
         "d.doi.org:10.12345/test",
+        "10.123/test",
     ),
 )
 def test_dois_that_should_not_validate(doi):
