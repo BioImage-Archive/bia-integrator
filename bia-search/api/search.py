e9925219316501ff9f1d930deab753deb37d98b7
Tushar <tushar@ebi.ac.uk>
2025-11-27 14:56:41 +0000

    search derived images

--- bia-search/api/search.py
+++ bia-search/api/search.py
@@ -121,7 +121,7 @@ async def fts(
 async def fts_image(
     elastic: Annotated[Elastic, Depends(get_elastic)],
     query: Annotated[str | None, Query()] = None,
-    includeDerivedImages: Annotated[bool | None, Query()] = False,
+    includeDerivedImages: Annotated[bool, Query()] = False,
     page: Annotated[int, Query(ge=1, alias="pagination.page", le=100)] = 1,
     page_size: Annotated[int, Query(ge=1, le=100, alias="pagination.page_size")] = 50,
 ) -> dict:
