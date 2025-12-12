9e440736d5d06064016f5469983e6dee895c1712
Francois <fsherwood@ebi.ac.uk>
2025-12-12 17:17:36 +0000

    Update curation to use new api persister

--- curation/tests/conftest.py
+++ curation/tests/conftest.py
@@ -1,66 +1,26 @@
-import os
-from pathlib import Path
-
 import bia_integrator_api.models as api_models
 import pytest
-from bia_integrator_api import ApiClient, Configuration, exceptions
-from bia_integrator_api.api import PrivateApi
+from bia_integrator_api import exceptions
 from bia_shared_datamodels import bia_data_model, mock_objects
-from dotenv import dotenv_values
+from persistence.bia_api_client import BIAAPIClient
+from persistence.utils import create_test_user, set_dev_settings_to_local
 
 from curation.settings import get_settings
 
 
-def test_user_creation_details() -> dict[str, str]:
-
-    user_creation_token = get_settings().local_user_create_secret_token
-    user_creation_details = {
-        "email": "test@example.com",
-        "password_plain": "test",
-        "secret_token": user_creation_token,
-    }
-    return user_creation_details
-
-
 def pytest_configure(config: pytest.Config):
-    env_settings = dotenv_values(str(Path(__file__).parents[1] / ".env_template"))
-    os.environ["bia_api_basepath"] = env_settings["local_bia_api_basepath"]
-    os.environ["bia_api_username"] = env_settings["local_bia_api_username"]
-    os.environ["bia_api_password"] = env_settings["local_bia_api_password"]
-
-
-def get_object_creation_client(
-    api_base_url: str,
-) -> PrivateApi:
-    api_config = Configuration(host=api_base_url)
-    private_api = PrivateApi(ApiClient(configuration=api_config))
-    user_dict = test_user_creation_details()
-    try:
-        access_token = private_api.login_for_access_token(
-            username=user_dict["email"], password=user_dict["password_plain"]
-        )
-    except exceptions.UnauthorizedException:
-        private_api.register_user(api_models.BodyRegisterUser(**user_dict))
-        access_token = private_api.login_for_access_token(
-            username=user_dict["email"],
-            password=user_dict["password_plain"],
-        )
-
-    assert access_token
-
-    api_config.access_token = access_token.access_token
-
-    return private_api
+    set_dev_settings_to_local()
 
 
 @pytest.fixture(scope="session")
-def private_client() -> PrivateApi:
-    setttings = get_settings()
-    return get_object_creation_client(setttings.local_bia_api_basepath)
+def private_client() -> BIAAPIClient:
+    settings = get_settings()
+    create_test_user(settings)
+    return BIAAPIClient(settings)
 
 
 @pytest.fixture(scope="session")
-def any_api_object(private_client: PrivateApi) -> api_models.Study:
+def any_api_object(private_client: BIAAPIClient) -> api_models.Study:
     study_dict = mock_objects.get_study_dict()
     study_object = api_models.Study.model_validate_json(
         bia_data_model.Study(**study_dict).model_dump_json()
